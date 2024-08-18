cdce36440191a0ac4c66421714aa3c267d0d1342
Mart van de Ven <m@type.hk>
2024-08-19 00:24:33 +0800

    clean up console.log

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,15 +1,12 @@
 import type { LayoutServerLoad } from './$types';
-import * as env from '$env/static/private';
-import * as public_env from '$env/static/public';
-console.log(env)
-console.log(public_env)
+import {PRIVATE_AUTH_GOOGLE_ID} from '$env/static/private';
 
 export const load: LayoutServerLoad = async (event) => {
 	return {
 		session: await event.locals.auth(),
 		authProviders: {
 			google: {
-				clientId: env.PRIVATE_AUTH_GOOGLE_ID
+				clientId: PRIVATE_AUTH_GOOGLE_ID
 			}
 		}
 	};
