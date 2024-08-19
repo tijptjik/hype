c92b9a413d0b3395dea1e507df63086fdf2f71f9
Mart van de Ven <m@type.hk>
2024-08-19 19:23:39 +0800

    DB --> D1DB

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaD1 } from '@prisma/adapter-d1';
 import { PrismaClient } from '@prisma/client';
 
-export const DB = (database: D1Database) => {
+export const D1DB = (database: D1Database) => {
 	const adapter = new PrismaD1(database);
 	return new PrismaClient({ adapter });
 };
 
-export default DB;
+export default D1DB;
