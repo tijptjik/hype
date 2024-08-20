7abde73faed1bca8329e323e542a1d8007fb721a
Mart van de Ven <m@type.hk>
2024-08-20 18:32:01 +0800

    Add type

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaD1 } from '@prisma/adapter-d1';
 import { PrismaClient } from '@prisma/client';
+import {type D1Database} from '@auth/d1-adapter';
 
 export const D1DB = (database: D1Database) => {
 	const adapter = new PrismaD1(database);
