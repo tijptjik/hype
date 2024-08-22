49d7be6240dd8facfcd4c8f14f7d531974c746e1
Mart van de Ven <m@type.hk>
2024-08-23 01:30:16 +0800

    Add API convenience functions

---
+++ src/lib/api.ts
@@ -0,0 +1,16 @@
+import { error, json } from '@sveltejs/kit';
+
+export const getSessionOrThrow = async (locals : App.Locals) => {
+	const session = await locals.auth();
+	if (!session?.user) {
+		throw error(401, 'No nice, no rice');
+	}
+	return session;
+}
+
+export const JSONResponseOrThrow = async (result : any): Promise<void> => {
+  if (!result) {
+  	throw error(404, "These aren't the signs you're looking for");
+  }
+  return json(result);
+}
