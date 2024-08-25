d778f1a76ccc737d33fd034d810c5ebda7733289
Mart van de Ven <m@type.hk>
2024-08-26 02:34:27 +0800

    Or just close the build xD

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,30 @@ import { defineConfig, type Plugin } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { migrateToLatest } from './src/lib/migrate';
 
+function ClosePlugin() : Plugin {
+    return {
+        name: 'ClosePlugin', // required, will show up in warnings and errors
+
+        // use this to catch errors when building
+        buildEnd(error) {
+            if(error) {
+                console.error('Error bundling')
+                console.error(error)
+                process.exit(1)
+            } else {
+                console.log('Build ended')
+            }
+        },
+
+        // use this to catch the end of a build without errors
+        closeBundle(id) {
+            console.log('Bundle closed')
+            process.exit(0)
+        },
+    } satisfies Plugin
+}
+
+
 const kyselyMigration = async ({ glob }: { glob: string }): Promise<Plugin> => {
 	return {
 		name: 'kysely-migration',
@@ -9,13 +33,14 @@ const kyselyMigration = async ({ glob }: { glob: string }): Promise<Plugin> => {
 		configResolved() {
 			migrateToLatest({ glob: glob });
 		}
-	} satisfies Plugin ;
+	} satisfies Plugin;
 };
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
 		// Migrations are defined in TSC, but migrations are run as JS to avoid import issues.
-		kyselyMigration({ glob: 'migrations/*.js' })
+		kyselyMigration({ glob: 'migrations/*.js' }),
+		ClosePlugin()
 	]
 });
