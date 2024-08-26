1c45e77080806b937f8df52b95934732ee97b048
Mart van de Ven <m@type.hk>
2024-08-26 22:13:36 +0800

    Health API without node

--- src/routes/api/health/+server.ts
+++ src/routes/api/health/+server.ts
@@ -2,32 +2,34 @@ import { error, type RequestHandler } from '@sveltejs/kit';
 import { JSONResponseOrThrow } from '$lib/api';
 
 function maskPrivateValues(obj: Record<string, string>): Record<string, string> {
-  const result: Record<string, string> = {};
+	const result: Record<string, string> = {};
 
-  for (const key in obj) {
-    if (key.startsWith("PRIVATE_") || key.startsWith("SECRET_") || key.endsWith("_TOKEN") || key.endsWith("_KEY")) {
-      result[key] = "***";
-    } else {
-      result[key] = obj[key];
-    }
-  }
-  return result;
+	for (const key in obj) {
+		if (key.startsWith('PRIVATE_') || key.startsWith('SECRET_') || key.endsWith('_TOKEN') || key.endsWith('_KEY')) {
+			result[key] = '***';
+		} else {
+			result[key] = obj[key];
+		}
+	}
+	return result;
 }
 
 // export default {
 export const GET: RequestHandler = async ({ locals, platform }) => {
 	// HTTP : 200 JSON or 404
 
+	const vars = import.meta.env.VITE_WRANGLER_ENV === 'local' ? process.env : import.meta.env;
+
 	try {
 		// HTTP : 200 JSON or 404
 		return JSONResponseOrThrow({
-			env: maskPrivateValues(process.env),
+			env: maskPrivateValues(vars),
 			locals: locals,
-			env_platform : Object.keys(platform?.env)
+			env_platform: Object.keys(platform?.env)
 		});
 	} catch (e) {
 		// DB : Query Error
 		console.error('Database query error:', e);
-		throw error(500, 'Internal Server Error');
+		throw error(500, 'Eternal Unhappiness Error');
 	}
 };
