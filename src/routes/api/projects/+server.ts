9b825b625d546004312ecf0d897222b74f4f21af
Mart van de Ven <m@type.hk>
2024-08-28 02:52:37 +0800

    Add projects endpoint

---
+++ src/routes/api/projects/+server.ts
@@ -0,0 +1,21 @@
+import { error, type RequestHandler } from '@sveltejs/kit';
+import { getSessionOrThrow, JSONResponseOrThrow } from '$lib/api';
+import client from '$lib/db';
+
+export const GET: RequestHandler = async ({ locals, platform }) => {
+  // AUTH : Pass or Fail
+  await getSessionOrThrow(locals);
+  // DB : Connect to D1
+  const db = client(platform?.env.DB);
+  try {
+    // DB : Build & Execute Query
+    const result = await db.query.geoProjects.findMany();
+    // HTTP : 200 JSON or 404
+    return JSONResponseOrThrow(result);
+  } catch (e) {
+    // DB : Query Error
+    console.error('Database query error:', e);
+    // HTTP : 500 Error
+    throw error(500, 'Dust Accumulation Critical');
+  }
+};
