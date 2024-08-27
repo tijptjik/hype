92135961dd0751d140d26a19c22d6c943fb9e5dc
Mart van de Ven <m@type.hk>
2024-08-28 02:57:30 +0800

    Add SQL script to truncate tables

---
+++ scripts/truncate.sql
@@ -0,0 +1,23 @@
+delete
+from account;
+
+delete
+from d1_migrations;
+
+delete
+from geoFeature;
+
+delete
+from geoCollection;
+
+delete
+from geoProject;
+
+delete
+from session;
+
+delete
+from userActivity;
+
+delete
+from user;
