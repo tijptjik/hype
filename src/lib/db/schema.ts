2eac5462a36333b964419f21a0014d00e72305e9
Mart van de Ven <m@type.hk>
2024-08-28 17:10:17 +0800

    Add organisation, organisationI18n, and organisationRole tables

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -27,6 +27,10 @@ export const user = sqliteTable('user', {
     .notNull()
 });
 
+export const userRelations = relations(user, ({ many }) => ({
+  memberships: many(organisationRole, { relationName: 'membership' })
+}));
+
 export const userActivity = sqliteTable('userActivity', {
   userId: text('userId')
     .primaryKey()
@@ -38,6 +42,107 @@ export const userActivity = sqliteTable('userActivity', {
     .default(sql`(strftime('%Y-%m-%dT%H:%M:%fZ', 'now'))`)
     .$onUpdate(() => new Date())
 });
+
+/* ----------------- */
+// ORGANISATIONS
+/* -------- */
+
+/* Organisations, have
+ * - NONE, ONE, or MANY `GeoProjects` that they own.
+ * - ONE or MANY `Owners`; associated users with the `OrganisationOwner` role
+ * - ONE or MANY `Members`; associated users with the `OrganisationMember` role
+ *
+ * Ownership does not imply membership, they both need to be defined.
+ *
+ * Users, have
+ * - NONE or ONE SuperAdmin role. Provides full rights in the app. Ben, Billy, Mart.
+ * - NONE, ONE, or MANY `OrganisationOwner` roles. Provides organisational admin rights.
+ * - NONE, ONE, or MANY `OrganisationMember` roles. Defines membership of `Organisation`(s).
+ * - NONE, ONE, or MANY `GeoProjectMaintainer` roles. Provides project edit rights.
+ *
+ * GeoProjects, have
+ * - ONE or MANY `Maintainers`; associated users with the `GeoProjectMaintainer` role.
+ *
+ * Only the Members of the Organisation which owns a GeoProject can be selected as its maintainer.
+ *  */
+
+export const organisation = sqliteTable('organisation', {
+  // Database identifier
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  // Public identifier
+  code: text('code').unique().notNull(),
+  // Full Name in English
+  name: text('name').notNull(),
+  // Short Name in English, used in navigation
+  nameShort: text('nameShort').notNull(),
+  // Description in English
+  description: text('description'),
+  url: text('url'),
+  image: text('image'),
+  createdAt: text('createdAt')
+    .default(sql`(strftime('%Y-%m-%dT%H:%M:%fZ', 'now'))`)
+    .notNull(),
+  modifiedAt: text('modifiedAt')
+    .default(sql`(strftime('%Y-%m-%dT%H:%M:%fZ', 'now'))`)
+    .$onUpdate(() => new Date())
+    .notNull()
+});
+
+export const organisationRelations = relations(organisation, ({ many }) => ({
+  members: many(organisationRole, { relationName: 'membership' })
+}));
+
+export const organisationI18n = sqliteTable(
+  'organisationI18n',
+  {
+    organisationId: text('organisationId')
+      .notNull()
+      .references(() => organisation.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
+    // IETF BCP 47 language tag
+    // https://www.rfc-editor.org/info/bcp47
+    lang: text('lang', { enum: ['zh-hant', 'zh-hans'] }).notNull(),
+    // Full Name in {lang}
+    name: text('name').notNull(),
+    // Short Name  in {lang}, used in navigation
+    nameShort: text('nameShort').notNull(),
+    // Description in {lang}
+    description: text('description')
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.organisationId, t.lang] })
+  })
+);
+
+export const organisationRole = sqliteTable(
+  'organisationRole',
+  {
+    organisationId: text('organisationId')
+      .notNull()
+      .references(() => organisation.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
+    userId: text('userId')
+      .notNull()
+      .references(() => user.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
+    role: text('role', { enum: ['member', 'owner'] })
+      .notNull()
+      .default('member')
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.organisationId, t.userId] })
+  })
+);
+
+export const organisationRoleRelations = relations(organisationRole, ({ one }) => ({
+  organisation: one(organisation, {
+    fields: [organisationRole.organisationId],
+    references: [organisation.id]
+  }),
+  user: one(user, {
+    fields: [organisationRole.userId],
+    references: [user.id]
+  })
+}));
 
 /* ----------------- */
 // AUTH
