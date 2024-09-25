aab5c26cf5a3d1d50c4f035518440f1937762ebb
Mart van de Ven <m@type.hk>
2024-09-25 08:30:27 +0800

    Move away from /form endpoints

--- src/routes/admin/organisations/[organisation]/+page.ts
+++ src/routes/admin/organisations/[organisation]/+page.ts
@@ -1,13 +1,18 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import { superValidate } from 'sveltekit-superforms';
+import { OrganisationSchema } from '$lib/db/schema';
+import { zod } from 'sveltekit-superforms/adapters';
 
 export const load: PageLoad = async ({ params, fetch }) => {
   const organisationCode = params.organisation;
-  const endPoint = `/api/organisations/${organisationCode}/form`;
+  const endPoint = `/api/organisations/${organisationCode}`;
 
   const request = await fetch(endPoint);
   if (request.status >= 400) return error(request.status);
 
-  const form = await request.json();
+  const formData: Record<string, unknown> = await request.json();
+  const form = await superValidate(formData, zod(OrganisationSchema));
+
   return { form };
 };
