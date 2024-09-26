8d26630e1c8612642b6da2edd5186c6e83fa4406
Mart van de Ven <m@type.hk>
2024-09-27 03:30:50 +0800

    Use genericIndexQuery() for projects

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -1,25 +1,46 @@
 import { error, type RequestHandler } from '@sveltejs/kit';
-import { getSessionOrError, JSONResponseOrError } from '$lib/api';
-import client from '$lib/db';
+import { getDatabaseOrError, JSONResponseOrError } from '$lib/api';
+import {
+  project,
+  projectRole,
+  projectI18n,
+} from '$lib/db/schema';
+import { genericIndexQuery } from '$lib/db';
+
+const RESOURCE_TYPE = 'projects';
+const ACCESS_STRATEGY = 'listingOwn';
 
 export const GET: RequestHandler = async ({ locals, platform }) => {
   // AUTH : Pass or Fail
-  await getSessionOrError(locals);
-  // DB : Connect to D1
-  const db = client(platform?.env.DB);
+  const { db, userId, accessStrategy } = await getDatabaseOrError(
+    locals,
+    platform,
+    ACCESS_STRATEGY,
+    RESOURCE_TYPE
+  );
+
   try {
     // DB : Build & Execute Query
-    const result = await db.query.project.findMany({
-      with: {
-        translations: true
-      }
-    });
+    const result = await genericIndexQuery(
+      db,
+      project,
+      {
+        translations: true,
+        maintainerRoles: true
+      },
+      projectRole,
+      projectI18n,
+      'projectId',
+      userId,
+      accessStrategy
+    );
+
     // HTTP : 200 JSON or 404
     return JSONResponseOrError(result);
   } catch (e) {
     // DB : Query Error
     console.error('Database query error:', e);
     // HTTP : 500 Error
-    throw error(500, 'Dust Accumulation Critical');
+    return error(500, 'Dust Accumulation Critical');
   }
 };
