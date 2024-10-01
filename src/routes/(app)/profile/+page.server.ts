2fa0cbb93fb6a43780b5e901bd9ec056e9fb672f
Mart van de Ven <m@type.hk>
2024-10-02 03:06:11 +0800

    return instead of throw

--- src/routes/(app)/profile/+page.server.ts
+++ src/routes/(app)/profile/+page.server.ts
@@ -4,6 +4,6 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (event) => {
   const session = await event.locals.auth();
   // Redirect if not authenticated
-  if (!session?.user) throw redirect(303, '/');
+  if (!session?.user) return redirect(303, '/');
   return { user: session.user };
 };
