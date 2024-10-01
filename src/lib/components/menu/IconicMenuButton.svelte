5467e4014b06269e20ad40d2b819400684c6bd5e
Mart van de Ven <m@type.hk>
2024-10-02 02:59:31 +0800

    Factor out IconicMenuButton Component

---
+++ src/lib/components/menu/IconicMenuButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { page } from '$app/stores';
+import { Icon } from '@steeze-ui/svelte-icon';
+
+let {
+  href,
+  iconSrc,
+  matchFromStart = true,
+  notificationCount = 0
+} = $props<{ href: string; iconSrc: any; matchFromStart?: boolean; notificationCount?: number }>();
+
+const isActive = () => {
+  if (matchFromStart) {
+    return $page.url.pathname.startsWith(href);
+  }
+  return $page.url.pathname.endsWith(href);
+};
+</script>
+
+<a {href} class="group btn btn-circle btn-ghost bg-inherit hover:bg-transparent {isActive() ? 'btn-active' : ''}">
+  {#if notificationCount > 0}
+    <div class="badge size-5 badge-sm badge-primary absolute -top-1 -right-1 text-xs">{notificationCount}</div>
+  {/if}
+  <div
+   class="border-b-2 border-double border-transparent py-2 transition-colors group-[.btn-active]:border-primary group-[.btn-active]:cursor-default group-[:not(.btn-active)]:group-hover:border-secondary">
+    <Icon size="24px" src={iconSrc} />
+  </div>
+</a>
