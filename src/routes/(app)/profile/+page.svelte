4d65b8a9eaad36ae7eabd0ff63fd0003e70c5996
Mart van de Ven <m@type.hk>
2024-10-02 03:06:40 +0800

    Prettify

--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -11,8 +11,8 @@ function formatDate(dateString: string): string {
 }
 </script>
 
-<div class="container mx-auto mb-12 mt-24 flex justify-center h-fit">
-  <div class="card min-h-64 max-w-128 bg-white text-black shadow-xl">
+<div class="container mx-auto mb-12 mt-24 flex h-fit justify-center">
+  <div class="max-w-128 card min-h-64 bg-white text-black shadow-xl">
     <div class="card-body flex flex-col">
       <h1 class="card-title">{user.name}</h1>
       <div class="space-y-2">
@@ -29,34 +29,39 @@ function formatDate(dateString: string): string {
           <h2 class="font-bold">{m.profile__memberships()}</h2>
           <small class="text-gray-500">{m.profile__memberships_note()}</small>
 
-          <h3 class="mt-3 mb-2 font-semibold">{m.profile__organisations()}</h3>
+          <h3 class="mb-2 mt-3 font-semibold">{m.profile__organisations()}</h3>
           <ul class="mt-2 space-y-2">
-            {#each user.roles.filter(role => role.type === 'organisations') as role}
+            {#each user.roles.filter((role) => role.type === 'organisations') as role}
               <li>
                 <div class="flex flex-wrap items-center">
-                  <span class="badge badge-outline mr-2 mb-1">{m[`profile__role_type__${role.role}`]()}</span>
-                  <a href="/admin/{role.type}/{role.resourceRef}" class="hover:underline break-all">{role.resourceName}</a>
+                  <span class="badge badge-outline mb-1 mr-2"
+                    >{m[`profile__role_type__${role.role}`]()}</span>
+                  <a href="/admin/{role.type}/{role.resourceRef}" class="break-all hover:underline"
+                    >{role.resourceName}</a>
                 </div>
               </li>
             {/each}
           </ul>
 
-          <h3 class="mt-4 mb-2 font-semibold">{m.profile__projects()}</h3>
+          <h3 class="mb-2 mt-4 font-semibold">{m.profile__projects()}</h3>
           <ul class="mt-2 space-y-2">
-            {#each user.roles.filter(role => role.type === 'projects') as role}
+            {#each user.roles.filter((role) => role.type === 'projects') as role}
               <li>
                 <div class="flex flex-wrap items-center">
-                  <span class="badge badge-outline mr-2 mb-1">{m[`profile__role_type__${role.role}`]()}</span>
-                  <a href="/admin/organisations/{role.parentRef}" class="hover:underline break-all">{role.parentName}</a>
+                  <span class="badge badge-outline mb-1 mr-2"
+                    >{m[`profile__role_type__${role.role}`]()}</span>
+                  <a href="/admin/organisations/{role.parentRef}" class="break-all hover:underline"
+                    >{role.parentName}</a>
                   <span class="mx-1">/</span>
-                  <a href="/admin/{role.type}/{role.resourceRef}" class="hover:underline break-all">{role.resourceName}</a>
+                  <a href="/admin/{role.type}/{role.resourceRef}" class="break-all hover:underline"
+                    >{role.resourceName}</a>
                 </div>
               </li>
             {/each}
           </ul>
         </div>
       {/if}
-      <div class="card-actions justify-end mt-4">
+      <div class="card-actions mt-4 justify-end">
         <button class="btn btn-secondary" onclick={() => signOut()}>{m.profile__logout()}</button>
       </div>
     </div>
