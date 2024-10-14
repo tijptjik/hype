e1fb041a116f706105085be6f1eefc9122246874
Mart van de Ven <m@type.hk>
2024-10-15 02:49:50 +0800

    Extend types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,6 @@
 export type ResourceTypes = 'organisation' | 'project' | 'layer' | 'feature';
+export type SectionTypes = 'core' | 'address' | 'images';
 export type FilterableResourceTypes = 'organisation' | 'project' | 'layer';
 export type ResourceFilters = { [key in FilterableResourceTypes]?: string[] };
-export type Resource = { id: string; nameShort: string; ref: string; description: string };
-export type ResourceWithData<T> = { data: T; id: string; nameShort: string; ref: string; description: string };
+export type Resource = { id: string; name: string; nameShort: string; ref: string; description: string };
+export type ResourceWithData<T> = { data: T; id: string; name: string; nameShort: string; ref: string; description: string };
