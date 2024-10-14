0c2d11a0c7ca5363516a03d0baf3b27dac8f7248
Mart van de Ven <m@type.hk>
2024-10-15 02:50:54 +0800

    Turn meta, resources, filterTexts, and queryFilters into stores

--- src/lib/stores/resources.svelte.ts
+++ src/lib/stores/resources.svelte.ts
@@ -1,18 +1,25 @@
-import type { ResourceTypes, ResourceWithData } from '$lib/types';
+import type { Resource, ResourceFilters, ResourceTypes, ResourceWithData } from '$lib/types';
 import { OrganisationSchema, ProjectSchema, LayerSchema, FeatureSchema } from '$lib/db/schema';
 
-// let resources = $state<{ [key in ResourceTypes]: Resource[] }>({
-//   organisation: [],
-//   project: [],
-//   layer: [],
-//   feature: []
-// });
-// let filterTexts = $state<{ [key in ResourceTypes]: string }>({
-//   organisation: '',
-//   project: '',
-//   layer: '',
-//   feature: ''
-// });
+// Resources
+
+export const resources = $state<{ [key in ResourceTypes]: Resource[] }>({
+  organisation: [],
+  project: [],
+  layer: [],
+  feature: []
+});
+export const filterTexts = $state<{ [key in ResourceTypes]: string }>({
+  organisation: '',
+  project: '',
+  layer: '',
+  feature: ''
+});
+export const queryFilters = $state<ResourceFilters>({
+  organisation: [],
+  project: [],
+  layer: []
+});
 export const filteredResources = $state<{
   organisation: ResourceWithData<typeof OrganisationSchema>[];
   project: ResourceWithData<typeof ProjectSchema>[];
@@ -24,3 +31,5 @@ export const filteredResources = $state<{
   layer: [],
   feature: []
 });
+export const meta = $state(
+  {'title' : 'Admin'});
