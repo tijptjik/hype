6dc8c41682b912bc22b55710b65c1efab295c8e0
Mart van de Ven <m@type.hk>
2024-10-15 02:51:15 +0800

    Factor out navItems into a store

---
+++ src/lib/stores/navigation.svelte.ts
@@ -0,0 +1,13 @@
+import {
+    Users as OrganisationIcon,
+    Squares2x2 as ProjectIcon,
+    Square3Stack3d as LayerIcon,
+    MapPin as FeatureIcon,
+  } from '@steeze-ui/heroicons';
+
+export const navItems = {
+    organisation: { name: 'Organisations', icon: OrganisationIcon, seq: 1, path: 'organisations' },
+    project: { name: 'Projects', icon: ProjectIcon, seq: 2, path: 'projects' },
+    layer: { name: 'Layers', icon: LayerIcon, seq: 3, path: 'layers' },
+    feature: { name: 'Features', icon: FeatureIcon, seq: 4, path: 'features' }
+  };
