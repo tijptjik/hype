a16f022807fd40280ae86b11763e1d2676358066
Mart van de Ven <m@type.hk>
2024-10-15 02:39:40 +0800

    Redirect to /core for organisational items

--- src/routes/admin/organisations/[organisation]/+page.ts
+++ src/routes/admin/organisations/[organisation]/+page.ts
@@ -1,18 +1,8 @@
-import { error } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
-import { superValidate } from 'sveltekit-superforms';
-import { OrganisationSchema } from '$lib/db/schema';
-import { zod } from 'sveltekit-superforms/adapters';
+import { redirect } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ params, fetch }) => {
-  const organisationCode = params.organisation;
-  const endPoint = `/api/organisations/${organisationCode}`;
+export function load() {
+  const currentPath = window.location.pathname;
+  const queryParams = window.location.search;
 
-  const request = await fetch(endPoint);
-  if (request.status >= 400) return error(request.status);
-
-  const formData: Record<string, unknown> = await request.json();
-  const form = await superValidate(formData, zod(OrganisationSchema));
-
-  return { form };
-};
+  redirect(307, `${currentPath}core${queryParams}`);
+}
