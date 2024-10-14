f4ba694f59f827e07b7826da8609ebeab86ae915
Mart van de Ven <m@type.hk>
2024-10-15 02:33:52 +0800

    Add active state and Header to admin layout

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+import { page } from '$app/stores';
+import { getActiveFromPath } from '$lib';
 import Sidebar from '$lib/components/layout/Sidebar.svelte';
 import Navbar from '$lib/components/layout/Navbar.svelte';
+import Header from '$lib/components/layout/Header.svelte';
+import { meta } from '$lib/stores/resources.svelte';
+import { navItems } from '$lib/stores/navigation.svelte';
 
 // PROPS
 let { children } = $props();
+
+const active = $derived(() => {
+  return getActiveFromPath($page.url.pathname);
+});
+
+// STATE : DERIVED
+$effect(() => {
+  if (active().ref === false) {
+    meta.title = (navItems[active().resourceType as keyof typeof navItems].name);
+  }
+});
+
 </script>
 
 <!-- LAYOUT -->
@@ -12,6 +29,7 @@ let { children } = $props();
   <header class="w-full flex-none bg-black">
     <Navbar />
   </header>
+  <Header {active} />
   <main
     class="flex w-full flex-auto overflow-y-auto bg-gradient-to-bl from-rose-500 to-fuchsia-800 bg-fixed">
     {@render children()}
