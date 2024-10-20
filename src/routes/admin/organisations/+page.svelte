c69d66e4292fd5c649b05dc9ad5f616742c944bd
Mart van de Ven <m@type.hk>
2024-10-20 12:42:23 +0800

    Add ResourceHeader

--- src/routes/admin/organisations/+page.svelte
+++ src/routes/admin/organisations/+page.svelte
@@ -1,42 +1,43 @@
 <script lang="ts">
 import { filteredResources } from '$lib/stores/resources.svelte';
-
-$effect(() => {
-  console.log('filteredResources', filteredResources);
-});
-
-const organisations = $derived(
-  filteredResources.organisation
-);
+import ResourceHeader from '$lib/components/layout/ResourceHeader.svelte';
+  
+const organisations = $derived(filteredResources.organisation);
 
 const RESOURCE_TYPE = 'organisation';
 </script>
 
-<div class="container mx-auto p-4">
-  <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-    {#each organisations as { data: org }}
-      <div class="card bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://placehold.co/600x400?text={org.name}"
-            alt={org.name}
-            class="h-48 w-full object-cover" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">
-            {org.name} <small class="text-sm text-gray-500">{org.nameShort}</small>
-          </h2>
-          <p class="mt-2">{org.description}</p>
-          <div class="mt-4 flex flex-row items-center justify-between">
-            <div class="flex flex-wrap gap-2">
-              {#each org.userRoles as userRole}
-                <span class="badge badge-outline">{userRole.role}</span>
-              {/each}
+<!-- LAYOUT -->
+<ResourceHeader />
+<div class="h-full overflow-y-auto bg-gradient-to-bl from-rose-500 to-fuchsia-800 bg-fixed">
+  <div class="container mx-auto flex w-full flex-auto p-4">
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {#each organisations as { data: org }}
+        <div class="card bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://placehold.co/600x400?text={org.name}"
+              alt={org.name}
+              class="h-48 w-full object-cover" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              {org.name} <small class="text-sm text-gray-500">{org.nameShort}</small>
+            </h2>
+            <p class="mt-2">{org.description}</p>
+            <div class="mt-4 flex flex-row items-center justify-between">
+              <div class="flex flex-wrap gap-2">
+                {#each org.userRoles as userRole}
+                  <span class="badge badge-outline">{userRole.role}</span>
+                {/each}
+              </div>
+              <!-- TODO Verify if the query params are retained -->
+              <a href="/admin/organisations/{org.code}" class="btn btn-primary"
+                >View Profile</a>
             </div>
-            <a href="/admin/organisations/{org.code}/core" class="btn btn-primary">View Profile</a>
           </div>
         </div>
-      </div>
-    {/each}
+      {/each}
+    </div>
   </div>
 </div>
