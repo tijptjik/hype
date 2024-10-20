24daf99988aa37f9db9187550ce64e43d398e9b1
Mart van de Ven <m@type.hk>
2024-10-20 12:29:35 +0800

    Add Form buttons

---
+++ src/lib/components/menu/FormResetButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import * as m from '$lib/paraglide/messages.js';
+import { Icon } from '@steeze-ui/svelte-icon';
+import { XCircle } from '@steeze-ui/heroicons';
+import { getForm } from '$lib/context/forms.svelte';
+
+const { tainted, isTainted, reset } = getForm();
+
+</script>
+<button
+  class="btn {!isTainted($tainted)
+    ? 'btn-disabled'
+    : 'btn-ghost'} pl-3 pr-4 disabled:bg-transparent disabled:text-opacity-60"
+  onclick={reset}
+  disabled={!isTainted($tainted)}>
+  <Icon src={XCircle} class="h-6 w-6" />
+  {m.forms__reset()}
+</button>
---
+++ src/lib/components/menu/FormSubmitButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import * as m from '$lib/paraglide/messages.js';
+import { getForm } from '$lib/context/forms.svelte';
+
+const { tainted, isTainted, submit } = getForm();
+
+</script>
+<button
+  class="btn {!isTainted($tainted)
+    ? 'btn-outline'
+    : 'btn-primary'} disabled:bg-transparent disabled:text-opacity-60"
+  onclick={submit}
+  disabled={!isTainted($tainted)}>
+  {m.forms__save()}
+</button>
