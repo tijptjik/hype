ec704e9340c3c06c6665d669bf24d51369dbc2bd
Mart van de Ven <m@type.hk>
2024-10-20 12:41:51 +0800

    Improve typing

--- src/routes/admin/features/+page.svelte
+++ src/routes/admin/features/+page.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
 import { filteredResources } from '$lib/stores/resources.svelte';
 import type { Feature } from '$lib/db/schema';
+import type { EntityWithData } from '$lib/types';
 
-let features = $derived(filteredResources.feature);
+let features = $derived<EntityWithData<Feature>[]>(filteredResources.feature);
 </script>
 
 <div class="container relative mx-auto h-screen overflow-y-auto p-4">
   <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-    {#each features as { data: feature as Feature }, idx}
+    {#each features as { data: feature }, idx}
       <div class="card bg-base-100 shadow-xl">
         <figure>
           <img
             src="https://picsum.photos/600/{400 + idx}"
-            alt={feature.properties.title}
+            alt={feature.properties?.title}
             class="h-48 w-full object-cover" />
         </figure>
         <div class="flex flex-row flex-wrap justify-center gap-2 py-2 align-middle">
           <span class="badge badge-primary">{feature.isPublished ? 'Published' : 'Draft'}</span>
-          <span class="badge badge-secondary">{feature.properties.material}</span>
+          <span class="badge badge-secondary">{feature.properties?.material}</span>
           <span class="badge badge-outline"
             >{feature.isVisitable ? 'Visitable' : 'Not Visitable'}</span>
         </div>
         <div class="card-body">
           <h2 class="card-title">
-            {feature.addressProperties.formattedAddress}
+            {feature.addressProperties?.formattedAddress}
             <small class="text-sm text-gray-500"
-              >{feature.properties.titleGen ? 'GenAI' : ''}</small>
+              >{feature.properties?.titleGen ? 'GenAI' : ''}</small>
           </h2>
-          <p class="mt-2">{feature.properties.description}</p>
+          <p class="mt-2">{feature.properties?.description}</p>
           <div class="mt-4 flex flex-row items-center justify-end">
             <a href="/admin/features/{feature.id}/core" class="btn btn-primary">View Details</a>
           </div>
