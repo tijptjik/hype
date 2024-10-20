70135c6e77e1a676a0fb2a29b11b5a4b55260f4c
Mart van de Ven <m@type.hk>
2024-10-20 12:24:25 +0800

    Clean up types

--- src/lib/stores/navigation.svelte.ts
+++ src/lib/stores/navigation.svelte.ts
@@ -1,15 +1,14 @@
 import {
-    Users as OrganisationIcon,
-    Squares2x2 as ProjectIcon,
-    Square3Stack3d as LayerIcon,
-    MapPin as FeatureIcon,
-  } from '@steeze-ui/heroicons';
-import type { ResourceTypes } from '$lib/types';
-import type { IconSource } from '@steeze-ui/heroicons';
+  Users as OrganisationIcon,
+  Squares2x2 as ProjectIcon,
+  Square3Stack3d as LayerIcon,
+  MapPin as FeatureIcon
+} from '@steeze-ui/heroicons';
+import type { ResourceToNavItem } from '$lib/types';
 
-export const navItems: { [key in ResourceTypes]: { name: string; icon: IconSource; seq: number; path: string } }  = {
-    organisation: { name: 'Organisations', icon: OrganisationIcon, seq: 1, path: 'organisations' },
-    project: { name: 'Projects', icon: ProjectIcon, seq: 2, path: 'projects' },
-    layer: { name: 'Layers', icon: LayerIcon, seq: 3, path: 'layers' },
-    feature: { name: 'Features', icon: FeatureIcon, seq: 4, path: 'features' }
-  };
+export const navItems: ResourceToNavItem = {
+  organisation: { name: 'Organisations', icon: OrganisationIcon, seq: 1, path: 'organisations' },
+  project: { name: 'Projects', icon: ProjectIcon, seq: 2, path: 'projects' },
+  layer: { name: 'Layers', icon: LayerIcon, seq: 3, path: 'layers' },
+  feature: { name: 'Features', icon: FeatureIcon, seq: 4, path: 'features' }
+};
