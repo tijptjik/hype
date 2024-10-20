0d3c845c691f708a0c3a12650c3c146a397bc605
Mart van de Ven <m@type.hk>
2024-10-20 13:34:00 +0800

    deconstruct single item arrays to object

--- src/routes/api/organisations/[code]/+server.ts
+++ src/routes/api/organisations/[code]/+server.ts
@@ -4,28 +4,27 @@ import { error, type RequestHandler } from '@sveltejs/kit';
 import {
   organisationRole,
   OrganisationSchema,
+  OrganisationDBSchema,
   organisation,
   organisationI18n,
   OrganisationI18n
 } from '$lib/db/schema';
-import {
-  getDatabaseOrError,
-  getSessionOrError,
-  JSONResponseOrError,
-  type AccessStrategyOption
-} from '$lib/api';
-import { genericProfileQuery } from '$lib/db';
+import { getDatabaseOrError, JSONResponseOrError, type AccessStrategyOption } from '$lib/api';
+import { genericEntityQuery } from '$lib/db';
 import { and, eq } from 'drizzle-orm';
 import type { z } from 'zod';
 import { actionResult } from 'sveltekit-superforms';
+import type { TargetLang } from '$lib/types';
 
 // Infer the type of OrganisationSchema
+type OrganisationDBType = z.infer<typeof OrganisationDBSchema>;
 type OrganisationType = z.infer<typeof OrganisationSchema>;
 type OrganisationI18nType = z.infer<typeof OrganisationI18n>;
 
 const RESOURCE_TYPE = 'organisation';
-const ACCESS_STRATEGY = 'profileOwn' as AccessStrategyOption;
+const ACCESS_STRATEGY = 'EntityOwn' as AccessStrategyOption;
 const PUBLIC_IDENTIFIER = 'code';
+
 
 export const GET: RequestHandler = async ({ params, locals, platform }) => {
   // AUTH : Pass or Fail
@@ -37,7 +36,7 @@ export const GET: RequestHandler = async ({ params, locals, platform }) => {
   );
   try {
     // DB : Build & Execute Query
-    const result = await genericProfileQuery(
+    const result = await genericEntityQuery(
       db,
       params[PUBLIC_IDENTIFIER] as string,
       PUBLIC_IDENTIFIER,
@@ -86,41 +85,46 @@ export const GET: RequestHandler = async ({ params, locals, platform }) => {
 
 export const PUT: RequestHandler = async ({ params, request, locals, platform }) => {
   // AUTH : Pass or Fail
-  await getSessionOrError(locals);
-  // DB : Connect to D1
-  const db = client(platform?.env.DB);
+  const { db, userId, accessStrategy } = await getDatabaseOrError(
+    locals,
+    platform,
+    ACCESS_STRATEGY,
+    RESOURCE_TYPE
+  );
+
   try {
     const formData: OrganisationType = await request.json();
-    const translations: OrganisationI18nType[] = formData.translations;
+    const formTranslations: Record<TargetLang, Omit<OrganisationI18nType, 'lang'>> = formData.translations || {};
 
     delete formData.translations;
 
     console.debug('formData', formData);
-    console.debug('translations', translations);
+    console.debug('translations', formTranslations);
 
-    const updatedOrganisation = await db
+    const [updatedOrganisation] = await db
       .update(organisation)
       .set({ ...formData })
       // @ts-ignore
       .where(eq(organisation.code, params.code))
       .returning();
 
-    if (updatedOrganisation.length === 0) {
+    if (!updatedOrganisation) {
       return error(404, 'Organisation not found');
     }
 
     const modifiedTranslations = [];
 
-    for (const translation of translations) {
+    for (let [lang, formTranslation] of Object.entries(formTranslations)) {
+      const translation = { ...formTranslation, lang: lang as 'zh-hant' | 'zh-hans' } as OrganisationI18nType;
       const existingTranslation = await db.query.organisationI18n.findFirst({
         where: and(
           eq(organisationI18n.organisationId, translation.organisationId),
-          eq(organisationI18n.lang, translation.lang)
+          eq(organisationI18n.lang, lang as 'zh-hant' | 'zh-hans')
         )
       });
 
       if (existingTranslation) {
-        const updatedTranslation = await db
+        const [updatedTranslation] = await db
           .update(organisationI18n)
           .set(translation)
           .where(
@@ -130,22 +134,21 @@ export const PUT: RequestHandler = async ({ params, request, locals, platform })
             )
           )
           .returning();
-        modifiedTranslations.push(updatedTranslation[0]);
+        modifiedTranslations.push(updatedTranslation);
       } else {
-        const insertedTranslation = await db
+        const [insertedTranslation] = await db
           .insert(organisationI18n)
           .values(translation)
           .returning();
-        modifiedTranslations.push(insertedTranslation[0]);
+        modifiedTranslations.push(insertedTranslation);
       }
     }
 
-    const rebuildForm = await superValidate(
-      {
-        ...updatedOrganisation,
-        translations: modifiedTranslations
-      },
-      zod(OrganisationSchema)
+    const rebuildForm = await superValidate({
+      ...updatedOrganisation,
+      translations: modifiedTranslations
+    },
+    zod(OrganisationSchema)
     );
 
     console.debug('rebuildForm', rebuildForm);
