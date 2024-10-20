9aafdba651b7afe3871f0120d2c3f5104ed2d626
Mart van de Ven <m@type.hk>
2024-10-20 12:20:43 +0800

    Add types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,32 @@
-export type ResourceTypes = 'organisation' | 'project' | 'layer' | 'feature';
-export type SectionTypes = 'core' | 'address' | 'images';
-export type FilterableResourceTypes = 'organisation' | 'project' | 'layer';
-export type ResourceFilters = { [key in FilterableResourceTypes]: string[] };
-export type Resource = { id: string; name: string; nameShort: string; ref: string; description: string };
-export type ResourceWithData<T> = { data: T; id: string; name: string; nameShort: string; ref: string; description: string };
+import type { IconSource } from '@steeze-ui/heroicons';
+
+export type ResourceType = 'organisation' | 'project' | 'layer' | 'feature';
+export type SourceLang = 'en'
+export type TargetLang = 'zh-hant' | 'zh-hans';
+export type ResourceToEntity = Record<ResourceType, Entity[]>;
+export type ResourceToText = Record<ResourceType, string>;
+export type FilterableResourceType = Exclude<ResourceType, 'feature'>;
+export type FilterableResourceToEntityId = Record<FilterableResourceType, string[]>;
+export type Id = string;
+export type Code = string;
+export type Ref = string;
+export type Entity = { id: Id; ref: Ref; name: string; nameShort: string; description: string };
+export type ApiEntity = Entity & {
+  code?: Code;
+  properties?: { title: string; description: string };
+};
+export type EntityWithData<T> = Entity & { data: T };
+export type FacetType = 'core' | 'address' | 'images';
+export type ResourceToNavItem = Record<ResourceType, NavItem>;
+
+export type Router = {
+  resource: ResourceType | false;
+  entity: Ref | false;
+  facet: FacetType | false;
+};
+export type NavItem = {
+  name: string;
+  icon: IconSource;
+  seq: number;
+  path: string;
+};
