c728c8e9417e504b4422162838b357813f784e9d
Mart van de Ven <m@type.hk>
2024-10-20 12:39:39 +0800

    Add logic to Facet MenuItem

--- src/lib/components/menu/MenuItem.svelte
+++ src/lib/components/menu/MenuItem.svelte
@@ -1,14 +1,41 @@
 <script lang="ts">
-const { label, href, isActive } = $props<{ label: string; href: string; isActive: boolean }>();
+import { getRouterState } from '$lib/context/router.svelte';
+import { page } from '$app/stores';
+
+// PROPS
+const { facet } = $props<{ facet: { ref: string; label: string } }>();
+
+// STATE : CONTEXT
+const routerState = getRouterState();
+
+// UTILS
+const getUrl = (facet: string) => {
+  const url = new URL($page.url.href);
+  url.hash = `#${facet}`;
+  return url.toString();
+};
+
+const onclick = (e: MouseEvent) => {
+  e.preventDefault();
+  const url = new URL(window.location.href);
+  url.hash = `#${facet.ref}`;
+  routerState.facet = facet.ref;
+  routerState.url = url;
+};
 </script>
 
 <li class="group">
   <a
-    href={href}
-    class="flex flex-col items-center uppercase px-4 py-2 transition-colors duration-200 ease-in-out hover:bg-transparent"
-    class:active={isActive}
-  >
-    <span>{label}</span>
-    <span class="w-full h-0.25 mt-1 transition-colors duration-200 ease-in-out {isActive ? 'bg-white' : 'bg-transparent group-hover:bg-white'}"></span>
+    href={getUrl(facet.ref)}
+    {onclick}
+    class="flex flex-col items-center px-4 py-2 uppercase transition-colors duration-200 ease-in-out hover:bg-transparent"
+    class:active={routerState.facet === facet.ref}>
+    <span>{facet.label}</span>
+    <span
+      class="mt-1 h-0.25 w-full transition-colors duration-200 ease-in-out"
+      class:bg-white={routerState.facet === facet.ref || (routerState.facet === false && facet.ref === "core")}
+      class:bg-transparent={routerState.facet !== facet.ref}
+      class:group-hover:bg-white={routerState.facet !== facet.ref}
+    ></span>
   </a>
 </li>
