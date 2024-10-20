e29897ed930924137fa7db2de3c2d0de8a7749ae
Mart van de Ven <m@type.hk>
2024-10-20 12:39:58 +0800

    Factor out the Form buttons

--- src/lib/components/menu/OrganisationActions.svelte
+++ src/lib/components/menu/OrganisationActions.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-import * as m from '$lib/paraglide/messages.js';
-import { Icon } from '@steeze-ui/svelte-icon';
-import { XCircle } from '@steeze-ui/heroicons';
+import FormResetButton from './FormResetButton.svelte';
+import FormSubmitButton from './FormSubmitButton.svelte';
 </script>
 
 <li>
-  <a class="btn btn-ghost pl-3 pr-4">
-    <Icon src={XCircle} class="h-6 w-6" />
-    {m.forms__reset()}
-  </a>
+  <FormResetButton />
 </li>
-<li><a class="btn btn-primary">{m.forms__save()}</a></li>
+<li>
+  <FormSubmitButton />
+</li>
