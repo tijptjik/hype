30f19417d925122f84752abc16d0eb1b61af6545
Mart van de Ven <m@type.hk>
2024-10-21 16:50:37 +0800

    Check nested errors

--- src/lib/components/menu/FormSubmitButton.svelte
+++ src/lib/components/menu/FormSubmitButton.svelte
@@ -1,18 +1,36 @@
 <script lang="ts">
-import * as m from '$lib/paraglide/messages.js';
-import { getForm } from '$lib/context/forms.svelte';
+  import * as m from '$lib/paraglide/messages.js';
+  import { getForm } from '$lib/context/forms.svelte';
+  import { CheckCircle } from '@steeze-ui/heroicons';
+  import { Icon } from '@steeze-ui/svelte-icon';
 
-const { tainted, isTainted, submit, errors } = getForm();
-
-const hasErrors = (): boolean => Object.keys($errors).length > 0;
+  // STATE : CONTEXT
+  const { tainted, isTainted, submit, errors} = getForm();
+  
+  // STATE : UI
+  let isInvalid = $state(false);
+  
+  // STATE : EFFECTS
+  $effect(() => {
+    isInvalid = (function checkErrors(obj) {
+      if (typeof obj !== 'object' || obj === null) {
+        return obj !== undefined;
+      }
+      return Object.values(obj).some(checkErrors);
+    })($errors);
+  });
 </script>
-
+  
 <button
-  class="btn disabled:bg-transparent disabled:text-opacity-60"
-  onclick={submit}
-  class:btn-primary={isTainted($tainted) && !hasErrors()}
-  class:btn-outline={!isTainted($tainted) || hasErrors()}
-  class:btn-error={hasErrors()}
-  disabled={!isTainted($tainted) || hasErrors()}>
-  {hasErrors() ? m.forms_invalid() : m.forms__save()}
+  class="btn disabled:bg-transparent disabled:text-opacity-60 transition-all duration-500"
+  onclick={(e) => submit(e)}
+  class:btn-primary={isTainted($tainted) && !isInvalid}
+  class:btn-outline={!isTainted($tainted) || isInvalid}
+  class:btn-error={isInvalid}
+  disabled={!isTainted($tainted) || isInvalid}>
+  <!-- TODO: add saved state -->
+  {#if false}
+    <Icon src={CheckCircle} />
+  {/if}
+  {isInvalid ? m.forms_invalid() : m.forms__save()}
 </button>
