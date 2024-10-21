3f57cbe1eb7f0d52b9aa3dfa82e4480d0c2098d5
Mart van de Ven <m@type.hk>
2024-10-22 00:30:30 +0800

    Hide button text on sm

--- src/lib/components/forms/FormUserActions.svelte
+++ src/lib/components/forms/FormUserActions.svelte
@@ -23,22 +23,26 @@ const toggleRemoveMode = (e: Event) => {
   {#if !searchMode}
     <button class="btn-rounded btn btn-ghost ml-auto bg-base-100" onclick={toggleRemoveMode}>
       {#if !removeMode}
-        <Icon src={UserPlus} class="mr-2 h-4 w-4" />
+        <Icon src={UserPlus} class="mr-2 h-4 w-4" /> <span class="hidden md:block"> 
         Remove User
+      </span>
       {:else}
-        <Icon src={XMark} class="h-4 w-4" />
+        <Icon src={XMark} class="h-4 w-4" /> <span class="hidden md:block">
         Stop Removing
+      </span>
       {/if}
     </button>
   {/if}
   {#if !removeMode}
     <button class="btn-rounded btn btn-ghost ml-auto bg-base-100" onclick={toggleSearch}>
       {#if !searchMode}
-        <Icon src={UserPlus} class="mr-2 h-4 w-4" />
+        <Icon src={UserPlus} class="mr-2 h-4 w-4" /> <span class="hidden md:block">
         Add User
+      </span>
       {:else}
-        <Icon src={XMark} class="h-4 w-4" />
+        <Icon src={XMark} class="h-4 w-4" /><span class="hidden md:block">
         Stop Adding
+      </span>
       {/if}
     </button>
   {/if}
