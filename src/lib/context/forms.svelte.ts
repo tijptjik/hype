13401af212d63b4d2c64b9d68acbb0508929683b
Mart van de Ven <m@type.hk>
2024-10-21 20:35:15 +0800

    Add support for New Organisations

--- src/lib/context/forms.svelte.ts
+++ src/lib/context/forms.svelte.ts
@@ -1,10 +1,10 @@
 import { zod } from "sveltekit-superforms/adapters";
 import { getContext, setContext } from 'svelte';
-import { OrganisationReqBody } from '$lib/db/zod';
+import { OrganisationReqBody, NewOrganisationReqBody } from '$lib/db/zod';
 import { superForm, superValidate } from 'sveltekit-superforms';
 import { defaults } from 'sveltekit-superforms';
 import { get } from "svelte/store";
-
+import { redirect } from "@sveltejs/kit";
 class OrganisationForm {
   form;
   enhance;
@@ -19,13 +19,13 @@ class OrganisationForm {
   message;
   posted;
 
-  constructor(data) {
+  constructor(data, isNew) {
     // ZodClient() works only with the same schema as the one used on the server. 
     // If you need to switch schemas on the client, you need the full adapter (for example zod instead of zodClient).
-    const formConfig = superForm(defaults(data.form.data, zod(OrganisationReqBody)), {
+    const formConfig = superForm(defaults(data.form.data, zod(isNew ? NewOrganisationReqBody : OrganisationReqBody)), {
       dataType: 'json',
       SPA: true,
-      validators: zod(OrganisationReqBody),
+      validators: zod(isNew ? NewOrganisationReqBody : OrganisationReqBody),
       validationMethod: 'auto',
       onSubmit: this.handleSubmit.bind(this),
       onResult: this.handleResult.bind(this)
@@ -38,11 +38,17 @@ class OrganisationForm {
     const result = await this.validateForm();
     const currentJsonData = get(this.form); // Get the current value of the form data
     const apiUrl = new URL(action.href);
-
     apiUrl.pathname = apiUrl.pathname.replace('/admin/', '/api/');
+    const isNew = apiUrl.pathname.endsWith('/new/');
+    const httpMethod = isNew ? 'POST' : 'PUT';
+
+    if (isNew) {
+      apiUrl.pathname = apiUrl.pathname.replace('/new/', '');
+    }
+
     
     const response = await fetch(apiUrl, {
-      method: 'PUT',
+      method: httpMethod,
       headers: {
         'Content-Type': 'application/json'
       },
@@ -51,9 +57,11 @@ class OrganisationForm {
 
     const formValidationResult = await superValidate(result.form, zod(OrganisationReqBody));
     Object.assign(this, formValidationResult);
-
-    if (!response.ok) {
-      console.error('Form submission failed');
+    
+    if (isNew) {
+      console.log('formValidationResult', formValidationResult);
+      console.log('redirecting to', `/admin/organisations/${formValidationResult.data.code}`);
+      redirect(302, `/admin/organisations/${formValidationResult.data.code}`);
     }
   }
 
@@ -68,5 +76,5 @@ class OrganisationForm {
 
 const FORM_STATE_KEY = Symbol('form');
 
-export const setForm = (data) => setContext(FORM_STATE_KEY, new OrganisationForm(data));
+export const setForm = (data, isNew=false) => setContext(FORM_STATE_KEY, new OrganisationForm(data, isNew));
 export const getForm = (): ReturnType<typeof setForm> => getContext(FORM_STATE_KEY);
