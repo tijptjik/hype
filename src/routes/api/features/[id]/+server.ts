caae0bc823517e3e227f4fcb3b6ad66b140250d3
Mart van de Ven <m@type.hk>
2024-10-25 02:14:23 +0800

    Add PUT for / /api/features/{id}

--- src/routes/api/features/[id]/+server.ts
+++ src/routes/api/features/[id]/+server.ts
@@ -1,15 +1,24 @@
 import { error, type RequestHandler } from '@sveltejs/kit';
-import {
-  projectRole
-} from '$lib/db/schema';
+import { superValidate, type SuperValidated } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
 import {
   getDatabaseOrError,
   JSONResponseOrError,
+  SuperFormErrorResponse,
+  SuperFormResponse,
   type AccessStrategyOption
 } from '$lib/api';
+// DB
 import { hierarchicalEntityQuery } from '$lib/db';
+import { updateFeature, extractEntitiesToUpdate, rebuildFormData } from '$lib/db/services/features';
+import { projectRole } from '$lib/db/schema';
+// ZOD
+import { FeatureUpdateAPI } from '$lib/db/zod';
+// TYPES
+import type { Feature } from '$lib/types';
 
 const RESOURCE_TYPE = 'feature';
+const RESOURCE_PATH = 'features';
 const ACCESS_STRATEGY = 'EntityOwnGrandChild' as AccessStrategyOption;
 const PUBLIC_IDENTIFIER = 'id';
 
@@ -42,5 +51,33 @@ export const GET: RequestHandler = async ({ params, locals, platform }) => {
     console.error('Database query error:', e);
     // HTTP : 500 Error
     return error(500, 'Dust Accumulation Critical');
+  }
+};
+
+export const PUT: RequestHandler = async ({ params, request, locals, platform }) => {
+  // AUTH : Pass or Fail
+  const { db, userId, accessStrategy } = await getDatabaseOrError(
+    locals,
+    platform,
+    ACCESS_STRATEGY,
+    RESOURCE_TYPE
+  );
+
+  try {
+    const formData: Feature = await request.json();
+    const form = await superValidate(formData, zod(FeatureUpdateAPI)) as SuperValidated<Feature>;
+
+    if (!form.valid) {
+      return SuperFormResponse(form);
+    }
+
+    const { baseFeature } = extractEntitiesToUpdate(form.data as Feature);
+    const updatedFeature = await updateFeature(db, baseFeature, params[PUBLIC_IDENTIFIER] as string);
+
+    const updatedForm = await rebuildFormData(updatedFeature);
+    return SuperFormResponse(updatedForm, false, false, RESOURCE_PATH, 200);
+  } catch (err) {
+    console.error(err);
+    return SuperFormErrorResponse(RESOURCE_TYPE);
   }
 };
