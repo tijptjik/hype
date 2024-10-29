229ce1825a764d4b4385a4a31cd2d84d733a4d74
Mart van de Ven <m@type.hk>
2024-10-29 14:21:06 +0800

    Add Select component

---
+++ src/lib/components/forms/FormSelect.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import Labels from './FormInputLabels.svelte';
+    // TYPES
+    import type { InputConstraints, InputConstraint } from 'sveltekit-superforms';
+    
+    // PROPS
+    type Props = {
+      id: string;
+      value: string;
+      values: string[];
+      constraints: InputConstraints<Record<string, InputConstraint>>;
+      languageTag: string;
+    };
+    
+    // STATE
+    let {
+      id,
+      value = $bindable(),
+      values,
+      constraints,
+      languageTag = 'core'
+    }: Props = $props();
+
+</script>
+    
+<select
+    {id}
+    name={id}
+    class="select w-full rounded-md bg-neutral p-2 border-none focus:border-none focus-within:border-none outline-none focus:outline-none focus-within:outline-none"
+    {...constraints}
+    bind:value={value}>
+    {#each values as optionValue}
+      <option value={optionValue}>{optionValue}</option>
+    {/each}
+</select>
