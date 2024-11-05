642c9c5b4bcef63f80d72c2aa477ff4d2beed876
Mart van de Ven <m@type.hk>
2024-11-05 19:03:06 +0800

    add getFieldComponent()

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,15 @@ import { page } from '$app/stores';
 import { browser } from '$app/environment';
 import deepEquals from 'fast-deep-equal';
 import { redirect } from '@sveltejs/kit';
+import InputField from '$lib/components/forms/FormFieldInput.svelte';
+import SelectField from '$lib/components/forms/FormFieldSelect.svelte';
+import TagsField from '$lib/components/forms/FormFieldTags.svelte';
+import RangeField from '$lib/components/forms/FormFieldRange.svelte';
+import TextareaField from '$lib/components/forms/FormFieldTextarea.svelte';
+import UsersField from '$lib/components/forms/FormFieldUsers.svelte';
+import CustomField from '$lib/components/forms/FormFieldProperties.svelte';
+import ComplexField from '$lib/components/forms/FormFieldComplex.svelte';
+import type { FieldComponentType } from './types';
 
 /**
  * Check whether the code is being run by a Cloudflare worker
@@ -37,3 +46,17 @@ export function toTitleCase(str: string) {
     (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
   );
 }
+
+export const getFieldComponent = (componentType?: FieldComponentType) => {
+  if (!componentType) return undefined;
+  return {
+    "InputField": InputField,
+    "SelectField": SelectField, 
+    "TagsField": TagsField,
+    "RangeField": RangeField,
+    "TextareaField": TextareaField,
+    "UsersField": UsersField,
+    "CustomField": CustomField,
+    "ComplexField": ComplexField
+  }[componentType];
+};
