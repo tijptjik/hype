36cdc37f851b62017ae2ebdd25a2719153c3da42
Mart van de Ven <m@type.hk>
2024-11-06 14:05:14 +0800

    Load props

--- src/lib/components/forms/FormFieldRange.svelte
+++ src/lib/components/forms/FormFieldRange.svelte
@@ -1,6 +1,44 @@
+<script module>
+  const name = 'FieldRange';
+</script>
+
 <script lang="ts">
 import ErrorLabel from './FormErrorLabel.svelte';
 // TYPES
+import type { InputConstraints, InputConstraint, ValidationErrors } from 'sveltekit-superforms';
+import type { ResourceType, FalsableRef, FalsableFacetType, FieldDiscriminator } from '$lib/types';
+import type { Writable } from 'svelte/store';
+import type { LanguageTag, Key, FormFieldExtendedDefinition } from '$lib/types';
+
+type Props = {
+  resourceType: ResourceType;
+  entity: FalsableRef;
+  facet: FalsableFacetType;
+  languageTag: LanguageTag;
+  fieldId: Key;
+  field: FormFieldExtendedDefinition;
+  fieldIndex: number;
+  fieldDiscriminator?: FieldDiscriminator;
+  fieldKey?: Key;
+  form: Writable<Form>;
+  constraints?: Writable<InputConstraints<Record<string, InputConstraint>>>;
+  errors?: Writable<ValidationErrors<Record<string, string>>>;
+};
+
+let {
+  resourceType,
+  entity,
+  facet,
+  languageTag = 'core',
+  fieldId,
+  field,
+  fieldIndex,
+  fieldDiscriminator,
+  fieldKey,
+  form,
+  constraints,
+  errors,
+}: Props = $props();
 
 </script>
 
