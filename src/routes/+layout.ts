4564f5b3c7bcfc528c93abfeb2e1c135e91988d2
Mart van de Ven <m@type.hk>
2024-11-11 17:30:22 +0800

    Never use a trailing slash

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,4 @@
 export const ssr = false;
 export const prerender = false;
 // See https://khromov.se/the-missing-guide-to-understanding-adapter-static-in-sveltekit/
-export const trailingSlash = 'always';
+export const trailingSlash = 'never';
