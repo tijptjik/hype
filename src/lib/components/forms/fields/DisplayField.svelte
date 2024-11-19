07bd9d5315122ded6ddd78844dc526a9a2039c32
Mart van de Ven <m@type.hk>
2024-11-19 18:51:44 +0800

    Add DisplayField

---
+++ src/lib/components/forms/fields/DisplayField.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import type { DisplayFieldProps } from '$lib/types';
+
+let { label, value, justify = 'start' }: DisplayFieldProps = $props();
+
+let displayLabel = $derived(
+  label.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => str.toUpperCase())
+);
+</script>
+
+<div
+  class="w-full bg-neutral first-child:rounded-t-xl last-child:rounded-b-xl"
+  class:text-right={justify === 'end'}>
+  <label class="block text-sm font-light text-base-content/70 px-3 pt-3"
+  style="font-variant: all-small-caps;">
+    {displayLabel}
+  </label>
+  <p class="text-base font-normal text-base-content px-3 pb-3">{value ?? '-'}</p>
+</div>
