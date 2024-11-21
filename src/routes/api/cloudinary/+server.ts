fa0c277dbcc99f78c87c2d2c7a64db563bc29170
Mart van de Ven <m@type.hk>
2024-11-21 12:11:41 +0800

    Add timestamp for signing

--- src/routes/api/cloudinary/+server.ts
+++ src/routes/api/cloudinary/+server.ts
@@ -5,14 +5,19 @@ import { error, json } from '@sveltejs/kit';
 export const POST = async ({ request }) => {
   const body = await request.json();
   const { paramsToSign } = body;
+  let timestamp = Date.now();
 
   try {
     const signature = cloudinary.utils.api_sign_request(
-      paramsToSign,
-      PRIVATE_CLOUDINARY_API_SECRET,
+      { timestamp },
+      PRIVATE_CLOUDINARY_API_SECRET
     );
-
-    return json({ signature });
+    return json({
+      signature,
+      timestamp,
+      cloudname: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME,
+      apikey: import.meta.env.VITE_CLOUDINARY_API_KEY
+    });
   } catch (e) {
     return error(500, `${error}`);
   }
