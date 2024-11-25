2bd916fad015c4cdb502b9c52a149537b76a60a6
Mart van de Ven <m@type.hk>
2024-11-26 04:36:41 +0800

    Handle image uploads for organisations and projects

--- src/routes/api/images/+server.ts
+++ src/routes/api/images/+server.ts
@@ -13,6 +13,8 @@ import {
   getImageForOrganisation
 } from '$lib/db/services/image';
 import type { NewImage, Image, NewImageAPI } from '$lib/types';
+import { patchProject } from '$lib/db/services/project';
+import { patchOrganisation } from '$lib/db/services/organisation';
 
 const RESOURCE_TYPE = 'image';
 const ACCESS_STRATEGY = 'ResourceFromEditableProject';
@@ -40,7 +42,7 @@ export const GET: RequestHandler = async ({ url, locals, platform }) => {
           undefined,
           PRIVILEGED_STRATEGY
         )
-      : await getDatabaseOrError(locals, platform, ACCESS_STRATEGY, RESOURCE_TYPE);
+      : await getDatabaseOrError(locals, platform, PRIVILEGED_STRATEGY, RESOURCE_TYPE);
 
     let images;
     if (featureId) {
@@ -51,9 +53,6 @@ export const GET: RequestHandler = async ({ url, locals, platform }) => {
         accessStrategy,
         PRIVILEGED_STRATEGY
       );
-      console.log('images', images);
-      console.log('accessStrategy', accessStrategy);
-      console.log('PRIVILEGED_STRATEGY', PRIVILEGED_STRATEGY);
     } else if (projectId) {
       images = await getImageForProject(db, projectId);
     } else if (organisationId) {
@@ -71,52 +70,95 @@ export const GET: RequestHandler = async ({ url, locals, platform }) => {
 
 export const POST: RequestHandler = async ({ request, locals, platform }) => {
   const formData: NewImageAPI = await request.json();
-  let featureId = formData.featureImage?.featureId;
-  if (!featureId) {
-    error(400, 'Give me FeatureId, or give me death');
+  if (!formData.refType || !formData.refId) {
+    error(400, 'Feature, Project or Organisation -- there is no try');
   }
-
-  // AUTH : Pass or Fail
-  const { db, userId, accessStrategy } = await getDatabaseOrError(
-    locals,
-    platform,
-    ACCESS_STRATEGY,
-    RESOURCE_TYPE,
-    featureId,
-    checkProjectAccessForFeature,
-    undefined,
-    PRIVILEGED_STRATEGY
-  );
-
-  if (accessStrategy !== PRIVILEGED_STRATEGY) {
-    // Public user uploads should use the /api/tasks endpoint
-    error(403, 'Fat cat says no');
-  }
-
   try {
+    if (formData.refType === 'feature') {
+      let featureId = formData.featureImage?.featureId;
+      if (!featureId) {
+        error(400, 'Give me FeatureId, or give me death');
+      }
+
+      // AUTH : Pass or Fail
+      const { db, userId, accessStrategy } = await getDatabaseOrError(
+        locals,
+        platform,
+        ACCESS_STRATEGY,
+        RESOURCE_TYPE,
+        featureId,
+        checkProjectAccessForFeature,
+        undefined,
+        PRIVILEGED_STRATEGY
+      );
+
+      // Add contributor ID if not provided
+      if (!formData.contributorId) {
+        formData.contributorId = userId;
+      }
+
+      if (accessStrategy !== PRIVILEGED_STRATEGY) {
+        // Public user uploads should use the /api/tasks endpoint
+        error(403, 'Fat cat says no');
+      }
+
+      const { baseImage, relatedFeatureImage } = extractEntitiesToInsert(formData);
+
+      const createdImage = await createImage(db, baseImage);
+      const createdFeatureImage = await createFeatureImage(
+        db,
+        relatedFeatureImage,
+        createdImage.id
+      );
+
+      return json(
+        {
+          ...createdImage,
+          intent: createdFeatureImage.intent,
+          isPublished: createdFeatureImage.isPublished,
+          publishedAt: createdFeatureImage.publishedAt
+        },
+        { status: 201 }
+      );
+    }
+
+    // AUTH : Pass or Fail
+    const { db, userId, accessStrategy } = await getDatabaseOrError(
+      locals,
+      platform,
+      PRIVILEGED_STRATEGY,
+      RESOURCE_TYPE
+    );
+
     // Add contributor ID if not provided
     if (!formData.contributorId) {
       formData.contributorId = userId;
     }
 
-    const { baseImage, relatedFeatureImage } = extractEntitiesToInsert(formData);
-
+    const { baseImage } = extractEntitiesToInsert(formData);
     const createdImage = await createImage(db, baseImage);
-    const createdFeatureImage = await createFeatureImage(
-      db,
-      relatedFeatureImage,
-      createdImage.id
-    );
+    let updatedOwner;
 
-    return json(
-      {
-        ...createdImage,
-        intent: createdFeatureImage.intent,
-        isPublished: createdFeatureImage.isPublished,
-        publishedAt: createdFeatureImage.publishedAt
-      },
-      { status: 201 }
-    );
+    if (formData.refType === 'project') {
+      updatedOwner = await patchProject(
+        db,
+        formData.refId,
+        {
+          imageId: createdImage.id
+        },
+        'id'
+      );
+    } else if (formData.refType === 'organisation') {
+      updatedOwner = await patchOrganisation(
+        db,
+        formData.refId,
+        {
+          imageId: createdImage.id
+        },
+        'id'
+      );
+    }
+    return json({ ...createdImage, owner: updatedOwner }, { status: 201 });
   } catch (err) {
     console.error('Failed to create image:', err);
     return error(500, 'Failed to create image');
