76f03860eda5a74ebad1ac7c17ffdb829f82a504
Mart van de Ven <m@type.hk>
2024-11-26 04:40:10 +0800

    Correct keyMapping

--- src/routes/admin/features/+page.svelte
+++ src/routes/admin/features/+page.svelte
@@ -6,16 +6,13 @@ import ResourceHeader from '$lib/components/layout/ResourceHeader.svelte';
 import ResourceIndex from '$lib/components/layout/ResourceIndex.svelte';
 import EntityCard from '$lib/components/layout/EntityCard.svelte';
 import Image from '$lib/components/common/Image.svelte';
-// TYPES
-import type { EntityWithData, Layer } from '$lib/types';
-
 // CONFIG :: KEY MAP
 const keyMap = {
   id: 'id',
   title: 'title',
   subtitle: 'addressProperties.neighbourhood',
   description: 'displayAddress',
-  image: 'id', // Using id for placeholder image
+  image: 'image',
   badges: [
     { label: 'isPublished', variant: 'primary' },
     { label: 'isVisitable', variant: 'outline' }
@@ -33,20 +30,19 @@ const entities = $derived(filteredResources.feature);
     <EntityCard {entity} {keyMap}>
       {#snippet header(entity)}
         <Image
-          src="https://picsum.photos/384/{192 + idx}"
-          alt={entity.displayAddress}
+          src={`https://picsum.photos/384/${192 + idx}`}
+          alt={entity.data.displayAddress}
           layout="cover" />
       {/snippet}
-
       {#snippet badges(entity)}
         <div class="flex flex-row flex-wrap justify-center gap-2 pb-2 align-middle">
           <span class="badge badge-primary"
-            >{entity.isPublished ? 'Published' : 'Draft'}</span>
-          {#each entity.properties as property}
+            >{entity.data.isPublished ? 'Published' : 'Draft'}</span>
+          {#each entity.data.properties as property}
             <span class="badge badge-secondary">{property.property.label}</span>
           {/each}
           <span class="badge badge-outline">
-            {entity.isVisitable ? 'Visitable' : 'Not Visitable'}
+            {entity.data.isVisitable ? 'Visitable' : 'Not Visitable'}
           </span>
         </div>
       {/snippet}
