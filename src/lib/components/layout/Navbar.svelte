0346d704342219dda5f5c467a2cbbfb03855a02a
Mart van de Ven <m@type.hk>
2024-11-26 23:34:13 +0800

    Refactor Navbar

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -4,13 +4,13 @@ import * as m from '$lib/paraglide/messages.js';
 import { signIn, signOut } from '@auth/sveltekit/client';
 import { hasControlPanelAccess } from '$lib/auth/utils';
 import Icon from '$lib/components/common/Icon.svelte';
-import { Bars3 } from '@steeze-ui/heroicons';
-import { ComputerDesktop, InboxArrowDown } from '@steeze-ui/heroicons';
+import { Bars3, ComputerDesktop, InboxArrowDown } from '@steeze-ui/heroicons';
 import IconicMenuButton from '$lib/components/menu/IconicMenuButton.svelte';
 
 const { session } = $page.data;
 
 let isMenuOpen = $state(false);
+let notificationCount = $state(0);
 
 const toggleMenu = () => {
   isMenuOpen = !isMenuOpen;
@@ -23,124 +23,131 @@ const pathsMenuRight = [
 ];
 
 $effect(() => {
-  // Close menu when route changes
   isMenuOpen = false;
 });
 </script>
 
+{#snippet navLink(href, label, className = '')}
+  <a
+    draggable="false"
+    {href}
+    class="select-none {className}"
+    class:btn-active={$page.url.pathname.startsWith(href)}>
+    {label}
+  </a>
+{/snippet}
+
+{#snippet menuList(items)}
+  <ul class="menu menu-horizontal space-x-2 p-0">
+    {#each items as { href, label }}
+      <li>
+        {@render navLink(
+          href,
+          label,
+          'btn btn-ghost rounded-none border-b-2 hover:bg-black hover:border-b-primary'
+        )}
+      </li>
+    {/each}
+  </ul>
+{/snippet}
+
+{#snippet adminControls()}
+  {#if session && hasControlPanelAccess(session)}
+    <ul class="menu menu-horizontal hidden space-x-2 p-0 lg:flex">
+      <li>
+        <IconicMenuButton
+          href="/admin/tasks"
+          iconSrc={InboxArrowDown}
+          matchFromStart={false}
+          {notificationCount} />
+      </li>
+      <li>
+        <IconicMenuButton href="/admin" iconSrc={ComputerDesktop} />
+      </li>
+    </ul>
+    <div class="mx-2 hidden h-5 w-px bg-neutral-800 lg:block"></div>
+  {/if}
+{/snippet}
+
+{#snippet userMenu()}
+  {#if session}
+    <div class="dropdown dropdown-end h-12">
+      <div tabindex="0" role="button" class="avatar btn btn-circle btn-ghost">
+        <div class="w-10 rounded-full">
+          <img alt="Avatar" src={session.user?.image} referrerpolicy="no-referrer" />
+        </div>
+      </div>
+      <ul
+        class="menu dropdown-content menu-sm z-[1] mt-6 w-52 rounded-box bg-base-300 p-2 shadow">
+        {#if hasControlPanelAccess(session)}
+          <div class="block lg:hidden">
+          <li>
+            {@render navLink('/admin', m.navbar__admin(), 'justify-between')}
+          </li>
+          <li>
+            {@render navLink('/admin/tasks', m.navbar__tasks(), 'justify-between')}
+          </li>
+          <div class="divider"></div>
+        </div>
+        {/if}
+        <li>
+          {@render navLink('/profile', m.navbar__profile(), 'justify-between')}
+        </li>
+        <li class="lg:hidden">
+          {@render navLink('/about', m.navbar__about(), 'justify-between')}
+        </li>
+        <li>
+          <a draggable="false" onclick={() => signOut()} class="select-none">
+            {m.navbar__signout()}
+          </a>
+        </li>
+      </ul>
+    </div>
+  {:else}
+    <a draggable="false" class="btn btn-primary" onclick={() => signIn('google')}>
+      {m.navbar__signin()}
+    </a>
+  {/if}
+{/snippet}
+
 <nav class="navbar sticky top-0 z-10 bg-black px-6 py-4 shadow-md">
   <div class="navbar-start">
+    <!-- Mobile Menu -->
     <div class="dropdown lg:hidden">
       <label tabindex="0" class="btn btn-ghost" onclick={toggleMenu}>
         <Icon src={Bars3} class="h-6 w-6" />
       </label>
       {#if isMenuOpen}
         <ul
-          tabindex="0"
           class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
           {#each pathsMenuRight as { href, label }}
             <li>
-              <a
-                draggable="false"
-                {href}
-                class:select-none active={$page.url.pathname.startsWith(href)}>{label}</a>
+              {@render navLink(href, label)}
             </li>
           {/each}
         </ul>
       {/if}
     </div>
+    <!-- Desktop Menu -->
     <div class="hidden lg:flex">
-      <ul class="menu menu-horizontal space-x-2 p-0">
-        {#each pathsMenuRight as { href, label }}
-          <li>
-            <a
-              draggable="false"
-              {href}
-              class="btn btn-ghost select-none"
-              class:btn-active={$page.url.pathname.startsWith(href)}>
-              {label}
-            </a>
-          </li>
-        {/each}
-      </ul>
+      {@render menuList(pathsMenuRight)}
     </div>
   </div>
 
   <div class="navbar-center">
-    <a
-      draggable="false"
-      href="/"
-      class="btn btn-ghost text-2xl font-light hover:bg-black select-none">
-      {m.navbar__ghostmapper()}
-    </a>
+    {@render navLink(
+      '/',
+      m.navbar__ghostmapper(),
+      'btn btn-ghost text-2xl font-light bg-black'
+    )}
   </div>
 
   <div class="navbar-end space-x-2">
-    <ul class="hidden lg:flex menu menu-horizontal space-x-2 p-0 ">
-      <li>
-        <a
-          draggable="false"
-          href="/about"
-          class="btn btn-ghost select-none"
-          class:btn-active={$page.url.pathname === '/about'}>
-          {m.navbar__about()}
-        </a>
-      </li>
+    <ul class="menu menu-horizontal hidden space-x-2 p-0 lg:flex">
+      {@render menuList([{ href: '/about', label: m.navbar__about() }])}
     </ul>
-    <div class="hidden lg:block h-5 w-px bg-neutral-800 mx-2"></div>
-    {#if session && hasControlPanelAccess(session)}
-    <ul class="hidden lg:flex menu menu-horizontal space-x-2 p-0 ">
-          <li>
-            <IconicMenuButton href="/reviewQueue" iconSrc={InboxArrowDown} matchFromStart={false} notificationCount={7} />
-          </li>
-          <li>
-            <IconicMenuButton href="/admin" iconSrc={ComputerDesktop} />
-          </li>
-      </ul>
-      <div class="hidden lg:block h-5 w-px bg-neutral-800 mx-2"></div>
-    {/if}
-    {#if session}
-      <div class="dropdown dropdown-end h-12">
-        <div tabindex="0" role="button" class="avatar btn btn-circle btn-ghost">
-          <div class="w-10 rounded-full">
-            <img alt="Avatar" src={session.user?.image} referrerpolicy="no-referrer" />
-          </div>
-        </div>
-        <ul
-          tabindex="0"
-          class="menu dropdown-content menu-sm z-[1] mt-6 w-52 rounded-box bg-base-300 p-2 shadow">
-          <li>
-            <a
-              draggable="false"
-              href="/profile"
-              class="justify-between select-none">
-              {m.navbar__profile()}
-              <span class="badge">{m.navbar__new()}</span>
-            </a>
-          </li>
-          <li class="lg:hidden">
-            <a
-              draggable="false"
-              href="/about"
-              class="justify-between select-none">
-              {m.navbar__about()}
-            </a>
-          </li>
-          <li>
-            <a
-              draggable="false"
-              onclick={() => signOut()}
-              class="select-none">{m.navbar__signout()}</a>
-          </li>
-        </ul>
-      </div>
-    {:else}
-      <a
-        draggable="false"
-        class="btn btn-primary"
-        onclick={() => signIn('google')}>
-        {m.navbar__signin()}
-      </a>
-    {/if}
+    <div class="mx-2 hidden h-5 w-px bg-neutral-800 lg:block"></div>
+    {@render adminControls()}
+    {@render userMenu()}
   </div>
 </nav>
