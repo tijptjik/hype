5ed7b4689764d744de0a3b0ac9d5d756a0632cd6
Mart van de Ven <m@type.hk>
2024-11-26 11:51:10 +0800

    Add option to unset Selects

--- src/lib/components/forms/elements/Select.svelte
+++ src/lib/components/forms/elements/Select.svelte
@@ -20,6 +20,7 @@ let {
   name={id}
   class="select w-full rounded-md border-none bg-neutral p-2 px-4 outline-none focus-within:border-none focus-within:outline-none focus:border-none focus:outline-none"
   {onchange}>
+  <option value={undefined}>-- Unset --</option>
   {#if isComplex}
     {#each values as { id, value }}
       <option value={id} class="m-12">{value}</option>
