634265acc4fd275d985d051797c01d82b584b363
Mart van de Ven <m@type.hk>
2024-11-30 03:13:22 +0800

    Factor out Accept and Reject Buttons

---
+++ src/lib/components/common/buttons/Reject.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+// COMPONENTS
+import Icon from '$lib/components/common/Icon.svelte';
+import { XCircle } from '@steeze-ui/heroicons';
+
+let { onclick } = $props();
+</script>
+
+<button class="btn btn-ghost btn-sm hover:bg-transparent hover:text-rose-300" {onclick}>
+  <Icon src={XCircle} class="h-4 w-4" />
+  Reject
+</button>
