5b4b1f1acb40e10b24e1f61e3eb9fcf736ce82be
Mart van de Ven <m@type.hk>
2024-11-30 14:48:51 +0800

    Create page for task detail

---
+++ src/routes/admin/tasks/[task]/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+// CONTEXT
+import { getRouterState } from '$lib/context/router.svelte';
+// COMPONENTS :: COMMON
+import Image from '$lib/components/tasks/common/Image.svelte';
+// COMPONENTS :: LAYOUT
+import Task from '$lib/components/tasks/layout/EntityRoot.svelte';
+import TaskHeader from '$lib/components/tasks/layout/Header.svelte';
+import TaskMain from '$lib/components/tasks/layout/Main.svelte';
+import TaskFooter from '$lib/components/tasks/layout/Footer.svelte';
+import Title from '$lib/components/tasks/common/Title.svelte';
+// COMPONENTS :: ACTIONS
+import ReportedMissingActions from '$lib/components/tasks/actions/ReportedMissing.svelte';
+import NewPhotoActions from '$lib/components/tasks/actions/NewPhoto.svelte';
+import NewFeatureActions from '$lib/components/tasks/actions/NewFeature.svelte';
+// COMPONENTS :: CONTROLS
+import ReportedMissingControls from '$lib/components/tasks/controls/ReportedMissing.svelte';
+import NewFeatureControls from '$lib/components/tasks/controls/NewFeature.svelte';
+// COMPONENTS :: INFO
+import Info from '$lib/components/forms/extra/Info.svelte';
+import ReportedMissingContent from '$lib/components/tasks/info/ReportedMissing.svelte';
+import NewPhotoContent from '$lib/components/tasks/info/NewPhoto.svelte';
+import NewFeatureContent from '$lib/components/tasks/info/NewFeature.svelte';
+
+// TYPES
+import type { TaskAPI, EntityRouter, PageProps } from '$lib/types';
+
+let pageProps: PageProps<TaskAPI> = $props();
+let { task } = pageProps.data;
+
+// STATE : CONTEXT :: ROUTER
+const routerState = getRouterState() as EntityRouter;
+
+console.log('UPDATE ROUTER STATE', task.id);
+routerState.updateWith({
+  resource: 'task',
+  entity: task.id,
+  facet: false
+});
+
+// SYNC :: Await immediately resolved promise to react to value change.
+const forceUpdate = async (_) => {};
+let doRerender = $state(0);
+</script>
+
+{#await forceUpdate(doRerender) then _}
+  <!-- LAYOUT -->
+
+  <div
+    class="h-full overflow-y-auto bg-gradient-to-br from-rose-500 to-indigo-700 bg-fixed p-6">
+    <Task {task}>
+      <TaskHeader {task} isRoundedBottom={false}>
+        {#snippet Left()}
+          <Title {task} />
+        {/snippet}
+        {#snippet Right()}
+          {#if task.type === 'reportedMissing'}
+            <ReportedMissingActions {task} />
+          {:else if task.type === 'newPhoto'}
+            <NewPhotoActions {task} />
+          {:else if task.type === 'newFeature'}
+            <NewFeatureActions {task} />
+          {/if}
+        {/snippet}
+        <Info>
+          {#if task.type === 'reportedMissing'}
+            <ReportedMissingContent />
+          {:else if task.type === 'newPhoto'}
+            <NewPhotoContent />
+          {:else if task.type === 'newFeature'}
+            <NewFeatureContent />
+          {/if}
+        </Info>
+      </TaskHeader>
+      <TaskMain {task}>
+        <Image {task} />
+        {#if task.type === 'reportedMissing'}
+          <ReportedMissingControls {task} />
+        {:else if task.type === 'newFeature'}
+          <NewFeatureControls {task} />
+        {/if}
+      </TaskMain>
+      <!-- <TaskFooter>
+        <Gallery {task} />
+      </TaskFooter> -->
+    </Task>
+  </div>
+{/await}
