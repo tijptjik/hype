426a2004186553bfa5a5affcb606fdecf77955e0
Mart van de Ven <m@type.hk>
2024-11-30 14:56:38 +0800

    Add <DownloadImageButton>

---
+++ src/lib/components/images/DownloadImageButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Icon from '$lib/components/common/Icon.svelte';
+import { CloudArrowDown } from '@steeze-ui/heroicons';
+import { downloadImage } from '$lib/images/index.svelte';
+import type { GetImageAPI } from '$lib/types';
+
+type Props = {
+  image: GetImageAPI;
+  class?: string;
+};
+
+let { image, class: className = '' } = $props();
+</script>
+
+<button
+  class="btn btn-circle opacity-70 hover:text-neutral-content hover:opacity-100 hover:shadow-sm {className}"
+  onclick={(e) => downloadImage(e, image)}>
+  <Icon src={CloudArrowDown} class="h-6 w-6" />
+</button>
