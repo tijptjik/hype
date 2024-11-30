df259ef0f72218d269ec79a9a82f277c1d991c76
Mart van de Ven <m@type.hk>
2024-11-30 14:44:57 +0800

    Add reusable <IconAnchor>

---
+++ src/lib/components/common/IconAnchor.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import Icon from '$lib/components/common/Icon.svelte';
+    import type { Component } from 'svelte';
+    import type { IconSource } from '@steeze-ui/heroicons';
+    
+    type Props = {
+      position?: 'left' | 'right';
+      icon: IconSource;
+      class?: string;
+      children?: any;
+    };
+    
+    let { 
+      position = 'right',
+      icon,
+      class: className = '',
+      children
+    }: Props = $props();
+    
+    </script>
+    
+    <div class="group/anchor relative z-30 {className}">
+      <!-- Icon Button -->
+      <div
+        class="absolute bottom-0 {position}-0 flex items-center justify-center opacity-70 transition-opacity duration-200 group-hover/anchor:opacity-0">
+        <button class="btn btn-circle">
+          <Icon src={icon} class="h-6 w-6" />
+        </button>
+      </div>
+    
+      <!-- Revealed Content -->
+      <div class="opacity-0 transition-opacity duration-200 group-hover/anchor:opacity-100">
+        {@render children?.()}
+      </div>
+    </div>
