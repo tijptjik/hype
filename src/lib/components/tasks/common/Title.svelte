d521fb46731bfd6805d1c029534be3161d41105d
Mart van de Ven <m@type.hk>
2024-12-08 10:25:14 +0100

    Link feature ttitle to feature

--- src/lib/components/tasks/common/Title.svelte
+++ src/lib/components/tasks/common/Title.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
+// LIB
+import { goToEntity } from '$lib';
+// CONTEXT
+import { getRouterState } from '$lib/context/router.svelte';
 // COMPONENTS
 import Icon from '$lib/components/common/Icon.svelte';
 import { ChevronRight } from '@steeze-ui/heroicons';
 // TYPES
-import type { TaskAPI, TaskType } from '$lib/types';
+import type { TaskAPI, TaskType, EntityRouter } from '$lib/types';
 
 let { task }: { task: TaskAPI } = $props();
 
 console.log(task);
+
+const routerState = getRouterState() as EntityRouter;
 
 const typeDisplay: Record<TaskType, string> = {
   reportedMissing: 'Reported Missing',
@@ -20,7 +26,10 @@ const typeDisplay: Record<TaskType, string> = {
   <Icon src={ChevronRight} class="h-6 w-6" />
   <h3 class="text-lg">
     {typeDisplay[task.type as TaskType]}
-    <small class="pl-3 text-sm text-base-content/50"
-      ><span class="font-normal"></span> {task.feature?.title || ''}</small>
+    <a
+      href={`/admin/features/${task.feature?.id}`}
+      onclick={(e) => goToEntity(e, routerState, 'feature', task.feature?.id)}
+      class="pl-3 text-sm text-base-content/50">
+      {task.feature?.title || ''}</a>
   </h3>
 </div>
