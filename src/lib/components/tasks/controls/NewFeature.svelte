9c10a96824fe79d4692510750e2e1b4587c67c2c
Mart van de Ven <m@type.hk>
2024-12-08 11:05:36 +0100

    Stylise NewFeature Controls

--- src/lib/components/tasks/controls/NewFeature.svelte
+++ src/lib/components/tasks/controls/NewFeature.svelte
@@ -7,24 +7,41 @@ import type { TaskAPI } from '$lib/types';
 let { task }: { task: TaskAPI } = $props();
 </script>
 
-<aside class="flex w-[420px] flex-col rounded-lg bg-base-200 p-4">
-  <h3 class="mb-4 text-lg font-semibold">New Feature Details</h3>
-  <div class="flex-grow space-y-2">
-    <p><span class="font-medium">Title</span> {task.feature?.title || '-'}</p>
-    <p>
-      <span class="font-medium">Description</span>
-      {task.feature?.description || '-'}
-    </p>
-    <p>
-      <span class="font-medium">Address</span>
-      {task.feature?.displayAddress || '-'}
-    </p>
+<aside class="w-[420px] flex flex-col gap-3 rounded-lg bg-base-200 p-6">
+  <h3 class="flex-grow-0 flex-shrink-0 mb-6 text-xl font-bold text-base-content">New Feature Details</h3>
+
+  <div class="flex-grow-1 flex-shrink-0 stats stats-vertical w-full bg-base-200">
+    <div class="stat">
+      <div class="stat-title">Title</div>
+      <div class="stat-value text-lg">{task.feature?.title || '-'}</div>
+    </div>
+
+    <div class="stat">
+      <div class="stat-title">Description</div>
+      <div class="stat-value text-wrap text-lg">
+        {task.feature?.description || '-'}
+      </div>
+    </div>
+
+    <div class="stat">
+      <div class="stat-title">Address</div>
+      <div class="stat-value text-wrap text-lg">
+        {task.feature?.displayAddress || '-'}
+      </div>
+    </div>
   </div>
+
   {#if task.image?.longitude && task.image?.latitude}
-    <div class="mt-4 h-[200px]">
-      <Map coordinates={[task.image?.longitude, task.image?.latitude]} />
+    <div class="flex-grow mt-6 h-full">
+      <Map
+        coordinates={[
+          parseFloat(task.image?.longitude),
+          parseFloat(task.image?.latitude)
+        ]}
+        draggable={false}
+      />
     </div>
   {:else}
-    <p class="text-sm text-base-content/50">No GPS data captured</p>
+    <div class="mt-6 text-sm text-base-content/50">No GPS data captured</div>
   {/if}
 </aside>
