2c59ec384734e7b067d95ef973a9ae991066f896
Mart van de Ven <m@type.hk>
2025-03-22 00:26:35 +0800

    Use $effect for inputField

--- src/lib/components/forms/fields/Input.svelte
+++ src/lib/components/forms/fields/Input.svelte
@@ -21,18 +21,45 @@ let {
 // STATE : FORM
 let { form, constraints, errors } = fieldProps.form;
 
-// STATE : INTERMEDIATE
-let fieldValues = $derived(
-  getValues($form, field, languageTag, fieldRoot, fieldIndex, fieldKey) || {
-    value: '',
-    isGenAI: false
+// STATE : LOCAL
+let inputValue = $state('');
+let isGenAI = $state(false);
+
+// EFFECT : SYNC WITH FORM
+$effect(() => {
+  const formValues = getValues(
+    $form,
+    field,
+    languageTag,
+    fieldRoot,
+    fieldIndex,
+    fieldKey
+  );
+  if (formValues) {
+    inputValue = formValues.value;
+    isGenAI = formValues.isGenAI;
   }
-);
+  console.log($form.properties[0].propertyValueId);
+});
 
 // STATE : DERIVED
 let id = $derived(
   getId(field, fieldRoot, fieldIndex, fieldDiscriminator, fieldKey, languageTag)
 );
+
+// HANDLERS
+function handleChange() {
+  updateForm(
+    form,
+    field,
+    languageTag,
+    fieldRoot,
+    fieldIndex,
+    fieldKey,
+    inputValue,
+    isGenAI
+  );
+}
 </script>
 
 <label class="form-control w-full" for={id} aria-label={field.label}>
@@ -42,22 +69,12 @@ let id = $derived(
   <div
     class="group relative rounded-lg border-1 border-transparent bg-neutral pl-2 pr-3 focus-within:outline focus-within:outline-1 focus-within:outline-neutral-500">
     <FormInput
-      bind:value={fieldValues.value as string}
-      bind:isGenAI={fieldValues.isGenAI as boolean}
+      bind:value={inputValue}
+      bind:isGenAI
       {id}
       {languageTag}
       {...field}
-      onchange={() =>
-        updateForm(
-          form,
-          field,
-          languageTag,
-          fieldRoot,
-          fieldIndex,
-          fieldKey,
-          fieldValues.value as string,
-          false
-        )} />
+      onchange={handleChange} />
   </div>
   <ErrorLabel {errors} {field} {languageTag} {fieldRoot} {fieldIndex} {fieldKey} />
 </label>
