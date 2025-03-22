bb17e48cc421099082736af6a831465661f3d9f8
Mart van de Ven <m@type.hk>
2025-03-22 21:19:06 +0800

    Recalculate the marker distance when its moved

--- src/lib/components/forms/sections/Map.svelte
+++ src/lib/components/forms/sections/Map.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-// MapLibre
-import SpectralStyle from '$lib/map/style.json';
-// UTILS
-import { loadScript } from '$lib';
+// SERVICES
+import { calculateDistance } from '$lib/utils/geocoding';
 // COMPONENTS
 import Map from '$lib/components/common/Map.svelte';
 // TYPES
@@ -21,11 +19,27 @@ let lngLat = $derived($form.geometry?.coordinates);
 const syncUpCoordinates = (lngLat: number[]) => {
   form.update(($form) => {
     $form.geometry.coordinates = lngLat;
+    if (
+      $form.addressMeta &&
+      $form.addressMeta?.longitude &&
+      $form.addressMeta?.latitude
+    ) {
+      $form.addressMeta.distanceFromPoint = calculateDistance(
+        lngLat[0],
+        lngLat[1],
+        $form.addressMeta?.longitude,
+        $form.addressMeta?.latitude
+      );
+    }
     return $form;
   });
 };
 </script>
 
 <div class="relative h-full w-full flex-grow">
-  <Map coordinates={lngLat} dragEndCallback={syncUpCoordinates} {...sectionProps} />
+  <Map
+    coordinates={lngLat}
+    dragEndCallback={syncUpCoordinates}
+    {form}
+    {...sectionProps} />
 </div>
