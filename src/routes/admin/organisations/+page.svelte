09208ef3577f4d97d1c8d0c706b580efb100404a
Mart van de Ven <m@type.hk>
2025-04-17 10:53:23 +0800

    set resource on load

--- src/routes/admin/organisations/+page.svelte
+++ src/routes/admin/organisations/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-// LIB
-import { ADMIN_PATH } from '$lib/index';
 // CONTEXT
 import { getHierarchicalResourceState } from '$lib/context/resources.svelte';
 // COMPONENTS
 import ResourceHeader from '$lib/components/layout/ResourceHeader.svelte';
 import ResourceIndex from '$lib/components/layout/ResourceIndex.svelte';
 import EntityCard from '$lib/components/layout/EntityCard.svelte';
+// ENUMS
+import { HierarchicalResource } from '$lib/types';
 // TYPES
 import type { KeyMap } from '$lib/components/layout/EntityCard.svelte';
 
 // CONFIG :: KEY MAP
+const RESOURCE = HierarchicalResource.organisation;
 const keyMap: KeyMap = {
   id: 'code',
   title: 'name',
@@ -21,6 +22,7 @@ const keyMap: KeyMap = {
 
 // CONTEXT
 const resourceState = getHierarchicalResourceState();
+resourceState.setResource(RESOURCE);
 </script>
 
 <ResourceHeader />
