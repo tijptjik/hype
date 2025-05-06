41a4bed0f7046ebfe15e1f04c0fe3937756b29dd
Mart van de Ven <m@type.hk>
2025-05-06 22:23:37 +0800

    Add info panel to settings

--- src/lib/components/panels/Settings.svelte
+++ src/lib/components/panels/Settings.svelte
@@ -2,20 +2,32 @@
 // I18N
 import { m } from '$lib/i18n';
 // COMPONENTS
-import Panel from '$lib/components/layout/Panel.svelte';
 import Header from '$lib/components/panels/common/Header.svelte';
+import Panel from '$lib/components/layout/Panel.svelte';
+import Info from '$lib/components/panels/settings/Info.svelte';
 import Profile from '$lib/components/panels/settings/Profile.svelte';
 import Language from '$lib/components/panels/settings/Language.svelte';
 import Contributor from '$lib/components/panels/settings/Contributor.svelte';
 import DefaultMap from '$lib/components/panels/settings/DefaultMap.svelte';
 import Experimental from '$lib/components/panels/settings/Experimental.svelte';
+// STATE
+let isInfoOpen = $state(false);
+
+let handleToggleInfo = (e: MouseEvent | TouchEvent) => {
+  e.stopPropagation();
+  isInfoOpen = !isInfoOpen;
+};
 </script>
 
 <Panel position="right" scrollable={true}>
   <Header
     panel="settings"
     title={m.settings__title()}
-    subtitle={m.settings__subtitle()} />
+    onToggleInfo={(e) => {
+      handleToggleInfo(e);
+    }} />
+  <Info isOpen={isInfoOpen} />
+
   <Profile />
   <div class="flex h-[calc(100vh-206px)] flex-col">
     <div class="flex-grow-1 flex min-h-0 flex-shrink-0 flex-col">
---
+++ src/lib/components/panels/settings/Info.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { m } from '$lib/i18n';
+import { slide } from 'svelte/transition';
+let { isOpen = false } = $props<{
+  isOpen?: boolean;
+}>();
+</script>
+
+{#if isOpen}
+  <div class="flex flex-col gap-4 px-6 py-3" transition:slide>
+    <h2 class="text-xl font-bold text-base-content">{m.these_helpful_moose_bend()}</h2>
+    <h3 class="text-lg text-base-content">{m.topical_alive_dolphin_pinch()}</h3>
+    <p class="text-neutral-content">
+      {m.extra_yummy_midge_wish()}
+    </p>
+    <h3 class="text-lg text-base-content">{m.sunny_key_mammoth_blend()}</h3>
+    <p class="text-neutral-content">
+      {m.petty_steep_dove_clasp()}
+    </p>
+    <h3 class="text-lg text-base-content">{m.jumpy_broad_wolf_startle()}</h3>
+    <p class="text-neutral-content">
+      {m.sleek_main_shell_taste()}
+    </p>
+    <h3 class="text-lg text-base-content">{m.fair_sound_goat_enchant()}</h3>
+    <p class="text-neutral-content">
+      {m.trite_key_insect_tickle()}
+    </p>
+    <h3 class="text-lg text-base-content">{m.jumpy_dark_dog_nudge()}</h3>
+    <p class="text-neutral-content">{m.big_awake_grebe_seek()}</p>
+  </div>
+{/if}
