af505c8e9c72549cabf0a8594699b1b85a927f2a
Mart van de Ven <m@type.hk>
2025-05-22 01:55:34 +0800

    debug seed

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ const seedDrizzle = async (): Promise<Plugin> => {
       name: 'seed-drizzle',
       apply: 'serve',
       buildStart() {
-        seed(env);
+        seed(env, true);
       }
     } satisfies Plugin;
   }
