4df11c722ddd498e80e22ee8fe14021b8335dbda
Mart van de Ven <m@type.hk>
2025-06-10 13:53:33 +0800

    Clean up landing page init

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,27 +2,24 @@
 // I18N
 import { m } from '$lib/i18n';
 // AUTH
-// @ts-ignore
-import { signIn } from '@auth/sveltekit/client';
-// STORES
-import { page } from '$app/state';
-// CONTEXT
-import { getMapCtx } from '$lib/context/map.svelte';
+import { signIn, useSession } from '$lib/auth/client';
 
-// CONTEXT
-const { session } = page.data;
+const session = useSession();
 
-const mapCtx = getMapCtx();
-if (!session) {
-  mapCtx.startCircularFlight([114.157, 22.319304]);
-}
 </script>
 
-{#if !session}
+{#if !$session.isPending && !$session.data?.user}
   <div
     class="absolute left-1/2 top-[60%] z-50 -translate-x-1/2 -translate-y-1/2 text-left">
     <h1 class="mb-4 text-6xl font-bold text-white">GHOST MAPPERS</h1>
-    <button class="btn bg-white text-neutral-900" onclick={() => signIn('google')}>
+    <button class="btn bg-white text-neutral-900" onclick={() => {
+      signIn.social({ 
+        provider: 'google', 
+        callbackURL: window.location.origin + '/'
+      }).catch((error) => {
+        console.error('🔴 OAuth: Sign-in error:', error);
+      });
+    }}>
       <img src="/google.svg" alt="Google logo" class="mr-2 h-4 w-4" />
       {m.preauth__continue_with_google()}
     </button>
