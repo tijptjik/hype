c1a848b5c68b724a2aaa548cbf97710df80238c4
Mart van de Ven <m@type.hk>
2025-06-10 16:06:46 +0800

    Move Info Slider up

--- src/lib/components/forms/extra/Info.svelte
+++ src/lib/components/forms/extra/Info.svelte
@@ -54,7 +54,7 @@ $effect(() => {
       <div
         bind:this={panel}
         transition:slide={{ duration: 250 }}
-        class="absolute right-1 top-[68px] z-50 w-[34rem] rounded-b-xl border-4 border-t-0 {borderColor} bg-base-100 p-6 shadow-lg">
+        class="absolute right-1 top-[56px] z-50 w-[34rem] rounded-b-xl border-4 border-t-0 {borderColor} bg-base-100 p-6 shadow-lg">
         <div
           class="pointer-events-none absolute inset-x-0 top-0 h-4 bg-gradient-to-b from-black/10 to-transparent"
           aria-hidden="true">
