94ed7a78fd6e1d47d5d9e402408c38886af4489d
Mart van de Ven <m@type.hk>
2025-06-13 00:07:22 +0800

    chore: setup drizzle-kit studio for remote dbs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -12,10 +12,13 @@ const getCredentials = () => {
   const prod = {
     driver: 'd1-http',
     dbCredentials: {
-      ...crawledDbHelper.withCfCredentials(
-        process.env.CLOUDFLARE_ACCOUNT_ID,
-        process.env.CLOUDFLARE_D1_TOKEN
-      ).proxyCredentials
+      accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+      databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+      token: process.env.CLOUDFLARE_D1_TOKEN!,
+      // ...crawledDbHelper.withCfCredentials(
+      //   process.env.CLOUDFLARE_ACCOUNT_ID,
+      //   process.env.CLOUDFLARE_D1_TOKEN
+      // ).proxyCredentials
     }
   };
 
