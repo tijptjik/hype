3cfe8cfe18ae2c6d5e3027268add39e1a7ba7b91
Mart van de Ven <m@type.hk>
2025-06-12 13:46:07 +0800

    feat: add env vars to Platform

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,6 +3,7 @@ import type { ParaglideLocals } from '@inlang/paraglide-sveltekit';
 import type { Auth, SessionSession, SessionUser } from '$lib/auth';
 import type { DrizzleD1Database } from 'drizzle-orm/d1';
 import type { HubOpts } from '$lib/types';
+import type { Flash } from '$lib/types';
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
@@ -40,12 +41,21 @@ declare global {
     // interface PageState {}
     interface Platform {
       env: {
-        DB: D1Database;
-        // Auth secrets
-        PRIVATE_AUTH_SECRET: string;
-        PRIVATE_AUTH_GOOGLE_ID: string;
-        PRIVATE_AUTH_GOOGLE_SECRET: string;
-        PRIVATE_SUPERADMIN_USERID: string;
+        // Cloudflare Bindings
+        DB: DrizzleD1Database;
+        ASSETS: any;
+        // AUTH
+        AUTH_SECRET: string;
+        AUTH_GOOGLE_ID: string;
+        AUTH_GOOGLE_SECRET: string;
+        SUPERADMIN_USERID: string;
+        // CLOUDINARY
+        CLOUDINARY_API_KEY: string;
+        CLOUDINARY_API_SECRET: string;
+        CLOUDINARY_CLOUD_NAME: string;
+        CLOUDINARY_UPLOAD_PRESET: string;
+        // AZURE
+        AZURE_TRANSLATION_KEY: string;
       };
       context: {
         waitUntil(promise: Promise<any>): void;
