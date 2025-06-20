b3ff11dbc316d036b35ddbb98978ae7b505eb66b
Mart van de Ven <m@type.hk>
2025-06-21 01:51:25 +0800

    feat: style header for new layout

--- src/lib/components/sidebar/Header.svelte
+++ src/lib/components/sidebar/Header.svelte
@@ -9,18 +9,19 @@ import { getSidebarState } from '$lib/context/sidebar.svelte';
 const sidebarState = getSidebarState();
 </script>
 
-<div class="flex flex-shrink-0 flex-row items-center justify-between bg-black p-4">
+<div
+  class="flex h-[72px] flex-shrink-0 flex-row items-center justify-between bg-black p-4">
   {#if sidebarState.isVisuallyOpenState()}
-    <div class="invisible h-12 w-12"></div>
-    <div class="flex w-full items-center justify-center">
+    <div class="invisible h-6 w-6"></div>
+    <div class="flex w-full items-center justify-center p-1">
       <Icon src={Bolt} class="h-8 w-8 text-primary" />
     </div>
-    <button class="btn btn-circle btn-ghost" onclick={sidebarState.toggleSidebar}>
+    <button class="btn btn-circle btn-ghost m-0" onclick={sidebarState.toggleSidebar}>
       <Icon src={Menu} class="h-6 w-6" />
     </button>
   {:else}
     <div
-      class="flex w-full items-center justify-center py-2"
+      class="m-0 flex w-full items-center justify-center p-1"
       onclick={sidebarState.toggleSidebar}>
       <Icon src={Bolt} class="h-8 w-8 text-primary" />
     </div>
