f7dd26f41e1115da66bfe8c6517b8f54bce17767
Mart van de Ven <m@type.hk>
2025-06-21 02:00:06 +0800

    fix: styling

--- src/lib/components/images/gallery/overlays/Loading.svelte
+++ src/lib/components/images/gallery/overlays/Loading.svelte
@@ -6,6 +6,6 @@ import { fade } from 'svelte/transition';
   class="absolute inset-0 animate-pulse rounded-lg bg-base-200"
   transition:fade={{ duration: 200 }}>
   <div class="flex h-full w-full items-center justify-center">
-    <span class="loading loading-spinner loading-md"></span>
+    <span class="loading loading-ring loading-md"></span>
   </div>
 </div>
