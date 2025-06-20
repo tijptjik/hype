b87cc07a85b128f2b70da5c0238f7d9c590e789d
Mart van de Ven <m@type.hk>
2025-06-20 14:08:38 +0800

    feat: add settings panel to admin

--- src/lib/components/menu/UserMenu.svelte
+++ src/lib/components/menu/UserMenu.svelte
@@ -1,42 +1,22 @@
 <script lang="ts">
 // SVELTE
 import { fade } from 'svelte/transition';
-// NAVIGATION
-import { goto } from '$app/navigation';
 // I18N
 import { m } from '$lib/i18n';
 // AUTH
-import { signIn, signOut, useSession } from '$lib/auth/client';
-// ICONS
-import Icon from '$lib/components/common/Icon.svelte';
-import { Power } from '@steeze-ui/heroicons';
+import { signIn, useSession } from '$lib/auth/client';
+// CONTEXT
+import { getAppCtx } from '$lib/context/app.svelte';
 
 const session = useSession();
-let showPower = $state(false);
+const appCtx = getAppCtx();
 </script>
 
 {#if $session.data?.user}
-  <div
-    class="grid h-10 w-10 px-2"
-    onmouseenter={() => (showPower = true)}
-    onmouseleave={() => (showPower = false)}>
-    {#if showPower}
+  <div class="flex items-center gap-2">
+    <div class="grid h-10 w-10 px-2">
       <button
-        transition:fade={{ duration: 300 }}
-        class="btn btn-ghost btn-sm col-start-1 col-end-2 row-start-1 row-end-2 h-12 min-h-0 w-12 rounded-full p-0"
-        onclick={async () => {
-          await signOut({
-            fetchOptions: {
-              onSuccess: () => {
-                goto('/'); // redirect to login page
-              }
-            }
-          });
-        }}>
-        <Icon src={Power} class="h-5 w-5 stroke-2" />
-      </button>
-    {:else}
-      <button
+        onclick={() => appCtx.togglePanel('settings')}
         transition:fade={{ duration: 300 }}
         class="btn btn-ghost btn-sm col-start-1 col-end-2 row-start-1 row-end-2 h-12 min-h-0 w-12 rounded-full p-0">
         <div class="avatar">
@@ -48,7 +28,7 @@ let showPower = $state(false);
           </div>
         </div>
       </button>
-    {/if}
+    </div>
   </div>
 {:else}
   <button
