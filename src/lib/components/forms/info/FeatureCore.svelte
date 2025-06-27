8d621c1d541ab61c413a1738d0b6c860bab139b7
Mart van de Ven <m@type.hk>
2025-06-28 00:48:52 +0800

    chore: use translation strings

--- src/lib/components/forms/info/FeatureCore.svelte
+++ src/lib/components/forms/info/FeatureCore.svelte
@@ -1,48 +1,40 @@
-<section>
-  <h3 class="mb-2 font-bold">Actions</h3>
-  <p>The toggles control the public visibility of the feature:</p>
-  <ul class="ml-6 mt-2 list-disc space-y-2">
+<script lang="ts">
+import { m } from '$lib/i18n';
+</script>
+
+<section class="leading-7">
+  <h3 class="mb-2 font-bold">{m.tasty_agent_cheetah_kick()}</h3>
+  <p>{@html m.home_legal_trout_hurl()}</p>
+  <ul class="ml-6 mt-4 list-disc space-y-3">
     <li>
-      <span class="font-semibold">ACCESSIBLE & OPEN TO PUBLIC</span> - Feature is shown on
-      the map if its layer is active. Set true if, in ordinary circumstances, the feature
-      is accessible to the public without restrictions or permission. If the feature is temporarily
-      inaccessible, this should be false.
+      {@html m.sea_born_mammoth_promise()}
     </li>
     <li>
-      <span class="font-semibold">INTANGIBLE FEATURE</span> - Feature may be shown on the
-      map with the caveat that there is (no longer) a physical presence on site. This invites
-      visitors to experience the feature's practical, historic context - e.g. sites of historic
-      events; a striking contrast with the present; or a locality that is more salient than
-      the missing materiality of the feature.
+      {@html m.nimble_tense_guppy_devour()}
     </li>
   </ul>
-  <p class="mt-2 text-sm italic">
-    Note that is a feature is permanently unavailable AND there is no historic,
-    intangible value to referencing the site, then it should simply to
-    <u>unpublished</u> to delist it from all maps.
+  <p class="mt-4 italic text-base-content/70">
+    {@html m.orange_icy_slug_amaze()}
   </p>
 </section>
 
-<section>
-  <h3 class="mb-2 font-bold">Classification</h3>
+<section class="leading-7">
+  <h3 class="mb-2 font-bold">{m.feature__classification()}</h3>
   <p>
-    Categorical dimensions along which the features can be filtered on the map. Fields
-    are defined at the Collection level.
+    {m.weak_main_earthworm_thrive()}
   </p>
 </section>
 
-<section>
-  <h3 class="mb-2 font-bold">Specification</h3>
+<section class="leading-7">
+  <h3 class="mb-2 font-bold">{m.feature__specification()}</h3>
   <p>
-    Free-form text fields that provide additional context and references. Fields are
-    defined at the Collection level.
+    {m.less_new_crocodile_dust()}
   </p>
 </section>
 
-<section>
-  <h3 class="mb-2 font-bold">Map</h3>
+<section class="leading-7">
+  <h3 class="mb-2 font-bold">{m.feature__map()}</h3>
   <p>
-    Unlock the map by clicking on the top-right lock, then drag the map to relocate the
-    feature marker.
+    {m.zesty_solid_warbler_play()}
   </p>
 </section>
