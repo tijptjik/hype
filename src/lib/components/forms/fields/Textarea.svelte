18e25d801ec143fbd4d0ac64219b892fd5146a2c
Mart van de Ven <m@type.hk>
2025-06-28 02:03:40 +0800

    feat: reflect whether a field is human or machine translated in the input elements

--- src/lib/components/forms/fields/Textarea.svelte
+++ src/lib/components/forms/fields/Textarea.svelte
@@ -27,37 +27,57 @@ let fieldValues = $derived(
   }
 );
 
+// STATE : LOCAL
+let inputValue = $state('');
+let isGenAI = $derived(fieldValues.isGenAI);
+
 // STATE : DERIVED
 let id = $derived(
   getId(field, fieldRoot, fieldIndex, fieldDiscriminator, fieldKey, locale)
 );
+
+function syncToForm(isGenAI: boolean) {
+  updateForm(
+    form,
+    field,
+    locale as Locale,
+    fieldRoot,
+    fieldIndex,
+    fieldKey,
+    fieldValues.value as string,
+    isGenAI
+  );
+}
+// HANDLERS
+function handleChange(e: Event) {
+  syncToForm(false);
+}
+
+function handleToggleGenAI(e: MouseEvent) {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log('toggle genai');
+  syncToForm(!isGenAI);
+  isGenAI = !isGenAI;
+}
 </script>
 
 {#if !field.isTranslated && locale !== 'core' && locale !== 'en'}
   <!-- SPACER -->
-  <div class="h-[74px] w-full rounded-lg bg-neutral bg-opacity-10"></div>
+  <div class="h-[74px] w-full rounded-lg bg-glass-100 bg-opacity-10"></div>
 {:else}
   <label class="form-control w-full">
     <FieldLabel {field} {fieldRoot} {fieldIndex} {fieldKey} {constraints} />
     <div
-      class="relative rounded-lg border-none bg-neutral pl-0 pr-3 focus-within:outline-neutral-500">
+      class="caret-prite relative rounded-lg border-none bg-glass-100 pl-0 pr-3 focus-within:outline-neutral-500 focus-within:ring-2 focus-within:ring-primary">
       <FormTextArea
         bind:value={fieldValues.value as string}
-        bind:isGenAI={fieldValues.isGenAI as boolean}
+        {isGenAI}
         {id}
         {locale}
         {...field}
-        onchange={() =>
-          updateForm(
-            form,
-            field,
-            locale as Locale,
-            fieldRoot,
-            fieldIndex,
-            fieldKey,
-            fieldValues.value as string,
-            false
-          )} />
+        onToggleGenAI={handleToggleGenAI}
+        onchange={handleChange} />
     </div>
     <ErrorLabel {errors} {field} {locale} {fieldRoot} {fieldIndex} {fieldKey} />
   </label>
