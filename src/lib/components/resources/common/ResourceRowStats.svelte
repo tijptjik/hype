0cf370cd8fbc122018e4bf502996abd8ce980160
Mart van de Ven <m@type.hk>
2025-06-28 02:34:19 +0800

    refactor: break up FeatureRow into reusable components

---
+++ src/lib/components/resources/common/ResourceRowStats.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+// I18N
+import { m } from '$lib/i18n';
+// COMPONENTS
+import StatusStats from '$lib/components/features/stats/StatusStats.svelte';
+import TranslationStats from '$lib/components/features/stats/TranslationStats.svelte';
+import ContentStats from '$lib/components/features/stats/ContentStats.svelte';
+import ImageStats from '$lib/components/features/stats/ImageStats.svelte';
+import CategoryStats from '$lib/components/features/stats/CategoryStats.svelte';
+import SpecifierStats from '$lib/components/features/stats/SpecifierStats.svelte';
+// TYPES
+import type { Feature } from '$lib/types';
+import type { AppCtx } from '$lib/context/admin.svelte';
+
+let {
+  feature,
+  appCtx,
+  grapheme = ''
+}: { feature: Feature; appCtx: AppCtx; grapheme?: string } = $props();
+</script>
+
+<div
+  class="pointer-events-none flex items-center justify-center gap-8 @[62rem]/main:gap-8 @[74rem]/main:gap-9 @[86rem]/main:gap-10 @[98rem]/main:gap-12 @[120rem]/main:gap-20">
+  <div class="flex flex-col items-center justify-center gap-1">
+    <small class="text-xs text-base-content/60">{m.feature__graphemes()}</small>
+    <div class="tooltip" data-tip={grapheme}>
+      <p
+        class="w-[80px] truncate text-sm text-base-content @[62rem]/main:w-[120px] @[74rem]/main:w-[160px] @[86rem]/main:w-[200px]">
+        {grapheme || '-'}
+      </p>
+    </div>
+  </div>
+  <StatusStats {feature} {appCtx} />
+  <div class="hidden @[50rem]/main:block">
+    <TranslationStats {feature} {appCtx} />
+  </div>
+  <div class="hidden @[50rem]/main:block">
+    <ContentStats {feature} {appCtx} />
+  </div>
+  <div class="hidden @[62rem]/main:block">
+    <ImageStats {feature} {appCtx} />
+  </div>
+  <div class="hidden @[78rem]/main:block">
+    <CategoryStats {feature} {appCtx} />
+  </div>
+  <div class="hidden @[84rem]/main:block">
+    <SpecifierStats {feature} {appCtx} />
+  </div>
+</div>
---
+++ src/lib/components/resources/common/ResourceStatusBadge.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { m } from '$lib/i18n';
+let {
+  isPublished,
+  isPendingReview
+}: { isPublished: boolean; isPendingReview: boolean } = $props();
+</script>
+
+<span
+  class="badge w-[100px] rounded-lg border-none bg-glass-neutral px-3 py-5 text-xs uppercase"
+  class:text-orange-500={isPendingReview}
+  class:text-ok={isPublished}
+  class:text-error={!isPublished && !isPendingReview}>
+  {isPendingReview
+    ? m.tangy_zany_capybara_arise()
+    : isPublished
+      ? m.yummy_ornate_snail_bend()
+      : m.weak_super_guppy_nail()}
+</span>
