e46846201cd198a550bc0cf0044bed1d047a8fcb
Mart van de Ven <m@type.hk>
2025-06-28 02:35:59 +0800

    feat: add reusable resource components

---
+++ src/lib/components/resources/common/ResourceEmptyState.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+// I18N
+import { m } from '$lib/i18n';
+</script>
+
+<!-- Empty State -->
+<div class="flex h-full w-full items-center justify-center text-center">
+  <h2
+    class="w-full text-center text-xl text-base-content/70 transition-opacity delay-75 duration-500">
+    {m.omni__no_results()}
+  </h2>
+</div>
