0924506695f545c3dfc38f05ebbcb2315851df28
Mart van de Ven <m@type.hk>
2025-06-28 02:36:59 +0800

    refactor: merge ResourceHeader and EntityHeader and split into components

---
+++ src/lib/components/resources/headers/HeaderFormActions.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+// COMPONENTS
+import FormResetButton from '$lib/components/menu/buttons/FormResetButton.svelte';
+import FormSubmitButton from '$lib/components/menu/buttons/FormSubmitButton.svelte';
+import ArchiveButton from '$lib/components/menu/buttons/ArchiveButton.svelte';
+import PublishButton from '$lib/components/menu/buttons/PublishButton.svelte';
+// CONTEXT
+import { getAppCtx } from '$lib/context/app.svelte';
+
+// STATE : CONTEXT
+const appCtx = getAppCtx();
+
+// STATE : DERIVED
+let formCtx = $derived(appCtx.formCtx);
+</script>
+
+{#if formCtx}
+  <div class="flex flex-row space-x-2">
+    <FormResetButton form={formCtx} />
+    <FormSubmitButton form={formCtx} />
+    <ArchiveButton form={formCtx} />
+    <PublishButton form={formCtx} />
+  </div>
+{/if}
---
+++ src/lib/components/resources/headers/HeaderSearch.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+// COMPONENTS
+import FilterInput from '$lib/components/menu/FilterInput.svelte';
+// CONTEXT
+import { getAppCtx } from '$lib/context/app.svelte';
+import type { FirstClassResource } from '$lib/enums';
+
+// STATE : CONTEXT
+const appCtx = getAppCtx();
+
+// STATE : DERIVED
+let resourceType = $derived(appCtx.headerResourceType) as FirstClassResource;
+
+function focusFirstVirtualListItem(e: KeyboardEvent) {
+  e.preventDefault();
+  const firstItem = document.querySelector(
+    'svelte-virtual-list-row [tabindex]'
+  ) as HTMLElement;
+  if (firstItem) {
+    firstItem.focus();
+  }
+}
+</script>
+
+<div class="relative flex w-full flex-grow items-center justify-center">
+  {#if resourceType}
+    <FilterInput {resourceType} rounded={true} onTabOut={focusFirstVirtualListItem} />
+  {/if}
+</div>
