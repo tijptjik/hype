d8edd553768b455dea8cb8ec95ee6f8dd30e8604
Mart van de Ven <m@type.hk>
2025-06-28 14:12:18 +0800

    fix: use dynamic inline values

--- src/lib/components/panels/Stars.svelte
+++ src/lib/components/panels/Stars.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 // I18N
 import { m } from '$lib/i18n';
+// CONTEXT
+import { getAppCtx } from '$lib/context/app.svelte';
 // COMPONENTS
 import Panel from '$lib/components/layout/Panel.svelte';
 import Header from '$lib/components/panels/common/Header.svelte';
@@ -9,6 +11,9 @@ import WantToVisit from '$lib/components/panels/sections/WantToVisit.svelte';
 import HaveVisited from '$lib/components/panels/sections/HaveVisited.svelte';
 // TYPES
 import type { PanelProps } from '$lib/types';
+
+// CONTEXT
+const appCtx = getAppCtx();
 
 // STATE
 let isInfoOpen = $state(false);
@@ -41,7 +46,7 @@ let panelProps: PanelProps = $derived({
   panelType: 'stars',
   position: 'left',
   scrollable: false,
-  inline: true,
+  inline: appCtx.isAdmin(),
   isNarrow: false,
   isAdmin: false
 });
