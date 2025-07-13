fc923598cc7799416f7dd72fb8c3d23b2235ecad
Mart van de Ven <m@type.hk>
2025-07-13 13:21:40 +0800

    feat: add redirects for panel urls

---
+++ src/routes/(app)/settings/+page.ts
@@ -0,0 +1,9 @@
+// SVELTEKIT
+import { redirect } from '@sveltejs/kit';
+// TYPES
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async () => {
+  // Redirect to home page with settings panel open
+  throw redirect(302, '/?panel=settings');
+};
