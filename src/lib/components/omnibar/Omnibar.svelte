cd6608f70b23fc0010313a5fc0187e16e74339da
Mart van de Ven <m@type.hk>
2025-07-13 15:18:42 +0800

    refactor: use appCtx.getHorizontalOffset

--- src/lib/components/omnibar/Omnibar.svelte
+++ src/lib/components/omnibar/Omnibar.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
 import { browser } from '$app/environment';
 // CONTEXT
-import { getOmniContext } from '$lib/context/omni.svelte';
+import { getOmniCtx } from '$lib/context/omni.svelte';
 import { getAppCtx } from '$lib/context/app.svelte';
 // COMPONENTS
 import OmniSearchBar from './OmniSearchBar.svelte';
 import OmniNavigationBar from './OmniNavigationBar.svelte';
-// CONFIG
-import { MOBILE_MAX_WIDTH } from '$lib/index';
 
 // CONTEXT
-const omniCtx = getOmniContext();
+const omniCtx = getOmniCtx();
 const appCtx = getAppCtx();
 
 let showSearch = $derived(omniCtx.state.mode === 'search');
@@ -22,19 +20,12 @@ function handleEscape(event: KeyboardEvent) {
 }
 
 // Calculate offset based on visible panels
-let horizontalOffset = $derived(() => {
-  const { filters, prisms, stars, settings } = appCtx.state.isPanelOpen;
-  const leftPanelOpen = prisms || stars;
-  const rightPanelOpen = filters || settings;
-
-  if (browser && window.innerWidth < MOBILE_MAX_WIDTH) return 0;
-  return (leftPanelOpen ? 210 : 0) - (rightPanelOpen ? 210 : 0);
-});
+let horizontalOffset = $derived(appCtx.getHorizontalOffset());
 </script>
 
 <div
   class="relative z-40 mt-0 flex-shrink-0 flex-grow-0 select-none px-0 caret-transparent duration-300 w-120:mt-2 w-192:mt-6 w-192:px-4 w-320:mt-10 w-320:px-9"
-  style="transform: translateX({horizontalOffset()}px)"
+  style="transform: translateX({horizontalOffset}px)"
   onkeydown={handleEscape}>
   <div class="mx-auto min-w-[320px] max-w-[480px]">
     <div
