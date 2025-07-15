d7f2281736cf4c6e42b56538562d100037045997
Mart van de Ven <m@type.hk>
2025-07-16 02:49:53 +0800

    feat: refresh images when a user contributed state is exited from

--- src/lib/context/card.svelte.ts
+++ src/lib/context/card.svelte.ts
@@ -31,6 +31,9 @@ export class CardCtx {
   isNewMode = $derived(this.state.mode === FeatureCardMode.New);
   isMissingMode = $derived(this.state.mode === FeatureCardMode.Missing);
   isAddPhotoMode = $derived(this.state.mode === FeatureCardMode.AddPhoto);
+  isSubmissionSuccessMode = $derived(
+    this.state.mode === FeatureCardMode.SubmissionSuccess
+  );
 
   setMissingReason(reason: string) {
     this.userData.missingReason = reason;
@@ -38,52 +41,6 @@ export class CardCtx {
 
   getMissingReason() {
     return this.userData.missingReason;
-  }
-  // UTILS
-  /**
-   * Adds a photo to the feature card context
-   */
-  addPhoto(photo: UploadedPhoto) {
-    if (
-      this.getError() === m.validation__at_least_one_image() ||
-      this.getError() === m.validation__at_least_one_image_as_evidence()
-    ) {
-      this.resetError();
-    }
-    this.userData.photos.push(photo);
-  }
-
-  addPhotosFromFiles(files: File[]) {
-    files.forEach((file) => {
-      this.addPhoto({
-        file,
-        previewUrl: URL.createObjectURL(file)
-      });
-    });
-  }
-
-  /**
-   * Removes a photo from the feature card context
-   */
-  removePhoto(photo: UploadedPhoto) {
-    // Release the object URL to avoid memory leaks
-    URL.revokeObjectURL(photo.previewUrl);
-    this.userData.photos = this.userData.photos.filter((p) => p !== photo);
-  }
-
-  removePhotoAtIndex(index: number) {
-    // Release the object URL to avoid memory leaks
-    this.removePhoto(this.userData.photos[index]);
-  }
-
-  /**
-   * Clears all photos from the feature card context
-   */
-  clearPhotos() {
-    this.userData.photos.forEach((photo) => {
-      URL.revokeObjectURL(photo.previewUrl);
-    });
-    this.userData.photos = [];
   }
 
   setError(error: string) {
