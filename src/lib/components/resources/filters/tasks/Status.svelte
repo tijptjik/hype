816d42bee1ab8c5358d0cdcca4ef5bdba4cc29d8
Mart van de Ven <m@type.hk>
2025-08-01 15:23:02 +0800

    fix: don't use !isReviewed fetch for isReviewed state filter
    fixes #103

--- src/lib/components/resources/filters/tasks/Status.svelte
+++ src/lib/components/resources/filters/tasks/Status.svelte
@@ -12,6 +12,8 @@ import {
 } from '$lib/client/services/filters';
 // COMPONENTS
 import FilterToggle from '../FilterToggle.svelte';
+// ENUMS
+import { FirstClassResource } from '$lib/enums';
 // TYPES
 import type { TaskStatusFilterKey } from '$lib/types';
 
@@ -54,11 +56,18 @@ const statusFilters: Record<
     transformOffset={12}
     falseLabel={getFeatureTaskLabel(filterDef, false, filterDef.invertBoolean)}
     trueLabel={getFeatureTaskLabel(filterDef, true, filterDef.invertBoolean)}
-    onToggleFalse={() => toggleResourceFilterState(adminCtx, 'task', key, false)}
-    onToggleTrue={() => toggleResourceFilterState(adminCtx, 'task', key, true)}
+    onToggleFalse={() => {
+      toggleResourceFilterState(adminCtx, 'task', key, false);
+      adminCtx.appCtx.refresh(FirstClassResource.task);
+    }}
+    onToggleTrue={() => {
+      toggleResourceFilterState(adminCtx, 'task', key, true);
+      adminCtx.appCtx.refresh(FirstClassResource.task);
+    }}
     onToggleChange={() => {
       const nextState =
         currentValue === null ? true : currentValue === true ? false : null;
       setResourceFilterState(adminCtx, 'task', key, nextState);
+      adminCtx.appCtx.refresh(FirstClassResource.task);
     }} />
 {/each}
