d91a378bde0dfb4117705437d2f6a242e156a785
Mart van de Ven <m@type.hk>
2026-02-09 10:03:57 +0800

    chore: enable remote functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapterStatic from '@sveltejs/adapter-static';
-import adapterCloudflare from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapterStatic from '@sveltejs/adapter-static'
+import adapterCloudflare from '@sveltejs/adapter-cloudflare'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 // Build an SSR version of the app for Cloudflare, or a static version when targeting Android.
 const selectAdapter = () => {
@@ -8,19 +8,19 @@ const selectAdapter = () => {
     return adapterStatic({
       fallback: 'index.html',
       pages: 'build-static',
-      assets: 'build-static'
-    });
+      assets: 'build-static',
+    })
   } else {
     return adapterCloudflare({
       // See https://kit.svelte.dev/docs/adapter-cloudflare
       routes: {
         include: ['/*'],
-        exclude: ['<all>']
+        exclude: ['<all>'],
       },
-      precompress: true
-    });
+      precompress: true,
+    })
   }
-};
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -28,38 +28,41 @@ const config = {
   // for more information about preprocessors
   compilerOptions: {
     experimental: {
-      async: true
+      async: true,
     },
     runes: true,
     // disable all warnings coming from node_modules and all accessibility warnings
-    warningFilter: (warning) =>
+    warningFilter: warning =>
       !warning.filename?.includes('node_modules') &&
       !warning.code.startsWith('a11y') &&
       !warning.message.startsWith('State referenced in its own') &&
       !warning.message.startsWith('Unused CSS selector') &&
       !warning.message.startsWith('You are using Svelte 5.0.0') &&
-      warning.code !== 'css-unused-selector'
+      warning.code !== 'css-unused-selector',
   },
   preprocess: vitePreprocess(),
   kit: {
     adapter: selectAdapter(),
+    csrf: {
+      trustedOrigins: ['*'],
+    },
     env: {
       privatePrefix: 'PRIVATE',
-      publicPrefix: 'PUBLIC'
+      publicPrefix: 'PUBLIC',
     },
-    csrf: {
-      trustedOrigins: ['*']
-    }
+    experimental: {
+      remoteFunctions: true,
+    },
   },
   vitePlugin: {
     // Required to avoid runes mode being enforce for all node_modules
     // https://github.com/sveltejs/svelte/issues/9632#issuecomment-1825498213
     dynamicCompileOptions({ filename }) {
       if (filename.includes('node_modules')) {
-        return { runes: undefined }; // or false, check what works
+        return { runes: undefined } // or false, check what works
       }
-    }
-  }
-};
+    },
+  },
+}
 
-export default config;
+export default config
