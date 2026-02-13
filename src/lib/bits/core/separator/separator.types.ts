e99d319404ec70016ec21d9674c60f390cae39e1
Mart van de Ven <m@type.hk>
2026-02-13 21:44:56 +0800

    fix: consistent props API

--- src/lib/bits/core/separator/separator.types.ts
+++ src/lib/bits/core/separator/separator.types.ts
@@ -3,7 +3,10 @@ import type { HTMLAttributes } from 'svelte/elements'
 export type SeparatorOrientation = 'horizontal' | 'vertical'
 export type SeparatorVariant = 'default' | 'dot'
 
-export interface SeparatorProps extends HTMLAttributes<HTMLDivElement> {
+export interface SeparatorProps
+  extends Omit<HTMLAttributes<HTMLDivElement>, 'id' | 'style'> {
+  id?: string
+  style?: string
   orientation?: SeparatorOrientation
   decorative?: boolean
   variant?: SeparatorVariant
