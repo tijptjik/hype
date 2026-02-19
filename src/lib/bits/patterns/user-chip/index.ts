a90b51b9f003b8e2a2e50949c5f1545a06af4b42
Mart van de Ven <m@type.hk>
2026-02-19 15:57:44 +0800

    refactor: delete user-chip demo code

--- src/lib/bits/patterns/user-chip/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as UserChip } from './UserChip.svelte'
-export type { UserChipProps } from './userChip.types'
--- src/lib/bits/patterns/user-chip/userChip.types.ts
+++
@@ -1,6 +0,0 @@
-export interface UserChipProps {
-  name: string
-  subtitle?: string
-  src?: string | null
-  class?: string
-}
