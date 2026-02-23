6f0a05f35e84a8b0a92765319a55509566455a4c
Mart van de Ven <m@type.hk>
2026-02-23 10:32:56 +0800

    feat: style hub orgs

--- src/lib/bits/custom/card/index.ts
+++ src/lib/bits/custom/card/index.ts
@@ -1,6 +1,4 @@
-export * as Card from './components'
-export type {
-  CardWrapperProps,
-  CardRootProps,
-  CardAvatarProps,
-} from './card.types'
+export { default as Wrapper } from './components/Wrapper.svelte'
+export { default as Root } from './components/Root.svelte'
+export { default as Avatar } from './components/Avatar.svelte'
+export type { CardWrapperProps, CardRootProps, CardAvatarProps } from './card.types'
