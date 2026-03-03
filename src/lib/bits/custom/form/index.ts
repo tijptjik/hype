92b692e609f153caa95377e37167b503d239a72b
Mart van de Ven <m@type.hk>
2026-03-03 13:01:54 +0800

    feat: Add skeleton component

--- src/lib/bits/custom/form/index.ts
+++ src/lib/bits/custom/form/index.ts
@@ -2,6 +2,7 @@ export { default as SectionHeader } from './SectionHeader.svelte'
 export { default as FormDebug } from './FormDebug.svelte'
 export { default as TextArea } from './TextArea.svelte'
 export { default as TextInput } from './TextInput.svelte'
+export { default as Skeleton } from './Skeleton.svelte'
 export type { FormDebugProps } from './formDebug.types'
 export type {
   SectionHeaderAction,
