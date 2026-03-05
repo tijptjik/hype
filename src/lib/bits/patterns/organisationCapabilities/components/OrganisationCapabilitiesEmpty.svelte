27a71064b09664a81835db006ef1f36ad0e31641
Mart van de Ven <m@type.hk>
2026-03-05 19:09:47 +0800

    feat: add empty capabilities component

---
+++ src/lib/bits/patterns/organisationCapabilities/components/OrganisationCapabilitiesEmpty.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+// I18N
+import { m } from '$lib/i18n'
+// BITS COMPONENTS
+import { Button } from '$lib/bits'
+
+let { onEnterEditMode }: { onEnterEditMode: () => void } = $props()
+</script>
+
+<section class="bits-theme bits-form__section bits-form__capabilities-empty">
+  <h3 class="bits-form__capabilities-empty-title">
+    {m.admin__forms_capabilities_empty_title()}
+  </h3>
+  <p class="bits-form__capabilities-empty-description">
+    {m.admin__forms_capabilities_empty_description()}
+  </p>
+  <Button
+    text={m.admin__forms_capabilities_add()}
+    color="primary"
+    class="bits-form__capabilities-empty-cta"
+    onClick={onEnterEditMode}
+  />
+</section>
