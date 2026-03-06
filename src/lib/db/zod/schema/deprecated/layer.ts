14e1b203fb6d9b4fbffe9496712891c249b9aeaa
Mart van de Ven <m@type.hk>
2026-03-06 23:50:52 +0800

    refactor: deprecate zod schemas

---
+++ src/lib/db/zod/schema/deprecated/layer.ts
@@ -0,0 +1,131 @@
+// ZOD
+import { z } from 'zod'
+// DRIZZLE
+import { createSelectSchema, createInsertSchema, createUpdateSchema } from 'drizzle-zod'
+import { layer, layerI18n, layerProperty, userLayer } from '$lib/db/schema/index'
+// CONSTRAINTS
+import { getDefaultConstraints, getLocales } from '../constraints'
+// ZOD SCHEMAS
+import {
+  PropertyBase,
+  PropertyI18nBase,
+  PropertyInsertAPI,
+  PropertyValueBase,
+  PropertyValueI18nBase,
+} from './property'
+import { UserBasic } from '../user'
+// TYPES
+import type { LayerMetadata } from '$lib/types'
+
+/* ----------------- */
+// LAYER CORESCHEMAS
+/* -------- */
+
+export const LayerBase = createSelectSchema(layer)
+export const LayerInsert = createInsertSchema(layer).extend({
+  ...getDefaultConstraints(layer),
+  metadata: z.custom<LayerMetadata>().prefault({}),
+})
+export const LayerUpdate = createUpdateSchema(layer).extend({
+  ...getDefaultConstraints(layer),
+})
+
+/* ----------------- */
+// LAYER RELATIONAL SCHEMAS
+/* -------- */
+
+export const LayerI18nBase = createSelectSchema(layerI18n)
+export const LayerI18nInsert = createInsertSchema(layerI18n)
+  .extend({
+    ...getDefaultConstraints(layerI18n),
+  })
+  .omit({
+    layerId: true,
+  })
+export const LayerI18nUpdate = createUpdateSchema(layerI18n).extend({
+  ...getDefaultConstraints(layerI18n),
+})
+
+export const LayerPropertyBase = createSelectSchema(layerProperty)
+export const LayerPropertyInsert = createInsertSchema(layerProperty).omit({
+  layerId: true,
+})
+export const LayerPropertyInsertExtra = LayerPropertyInsert.extend({
+  property: PropertyInsertAPI.optional(),
+})
+
+export const LayerPropertyUpdate = createUpdateSchema(layerProperty)
+// Make property optional so it validates whether nested data is present or not
+export const LayerPropertyUpdateExtra = LayerPropertyUpdate.extend({
+  property: PropertyInsertAPI.optional(),
+})
+
+/* ----------------- */
+// USER RELATIONAL SCHEMAS
+/* -------- */
+
+export const UserLayerBase = createSelectSchema(userLayer)
+export const UserLayerInsert = createInsertSchema(userLayer).extend({
+  isVisibleOnLoad: z.boolean(),
+})
+export const UserLayerUpdate = createUpdateSchema(userLayer)
+
+/* ----------------- */
+// LAYER API SCHEMAS
+/* -------- */
+
+// Basic layer collection schema - optimized for performance
+export const LayerCollectionAPI = LayerBase.omit({
+  publisherId: true,
+  publishedAt: true,
+  createdAt: true,
+  modifiedAt: true,
+}).extend({
+  i18n: getLocales(LayerI18nBase),
+  properties: z.array(LayerPropertyBase),
+})
+
+// Basic layer entity schema - optimized for performance
+export const LayerAPI = LayerBase.omit({
+  createdAt: true,
+  modifiedAt: true,
+}).extend({
+  i18n: getLocales(LayerI18nBase),
+  properties: z.array(LayerPropertyUpdateExtra),
+  publisher: UserBasic.nullish(),
+})
+
+export const LayerInsertAPI = LayerInsert.extend({
+  i18n: getLocales(LayerI18nInsert),
+  properties: z.array(LayerPropertyInsertExtra),
+  publisher: UserBasic.nullish(),
+})
+
+export const LayerUpdateAPI = LayerUpdate.extend({
+  i18n: getLocales(LayerI18nUpdate),
+  properties: z.array(LayerPropertyUpdateExtra),
+  publisher: UserBasic.nullish(),
+})
+
+/* ----------------- */
+// LAYER RAW SCHEMAS
+/* -------- */
+
+export const LayerPropertyRaw = LayerPropertyBase.extend({
+  property: PropertyBase.extend({
+    i18n: z.array(PropertyI18nBase),
+    values: z
+      .array(
+        PropertyValueBase.extend({
+          i18n: z.array(PropertyValueI18nBase).optional(),
+        }),
+      )
+      .nullish()
+      .nullish(),
+  }).nullish(),
+})
+
+export const LayerRaw = LayerBase.extend({
+  i18n: z.array(LayerI18nBase),
+  properties: z.array(LayerPropertyRaw),
+})
