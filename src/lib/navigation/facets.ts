4737505b6a4e4b3ed985a123272070ed7735182f
Mart van de Ven <m@type.hk>
2026-03-08 10:32:53 +0800

    feat: add fields to organisatiosn

--- src/lib/navigation/facets.ts
+++ src/lib/navigation/facets.ts
@@ -55,7 +55,12 @@ export const ADMIN_SUPPORTED_FACETS_BY_RESOURCE: Partial<
   Record<FirstClassResource, readonly FacetType[]>
 > = {
   hub: [ADMIN_FACETS.core, ADMIN_FACETS.fields, ADMIN_FACETS.images],
-  organisation: [ADMIN_FACETS.core, ADMIN_FACETS.capabilities, ADMIN_FACETS.images],
+  organisation: [
+    ADMIN_FACETS.core,
+    ADMIN_FACETS.capabilities,
+    ADMIN_FACETS.fields,
+    ADMIN_FACETS.images,
+  ],
   project: [
     ADMIN_FACETS.core,
     ADMIN_FACETS.capabilities,
