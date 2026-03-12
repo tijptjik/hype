ec28b7b26314cd394688bc6b6e757909be88f281
Mart van de Ven <m@type.hk>
2026-03-12 18:52:34 +0800

    feat: add dropdown support to headertitle

--- src/lib/bits/patterns/layout/header/components/HeaderTitle.svelte
+++ src/lib/bits/patterns/layout/header/components/HeaderTitle.svelte
@@ -2,6 +2,8 @@
 // BITS COMPONENTS
 import { Header as BitsHeader } from '$lib/bits/custom'
 import { Dropdown } from '$lib/bits/core'
+// SVELTE
+import { slide } from 'svelte/transition'
 // TYPES
 import type { HeaderTitleProps } from './headerPrimitives.types'
 // ICONS
@@ -18,20 +20,7 @@ let {
   hideDescription = false,
 }: HeaderTitleProps = $props()
 
-const MenuIcon = $derived(menuAction?.icon ?? null)
-const menuItems = $derived(
-  menuAction
-    ? [
-        {
-          label: menuAction.label,
-          onSelect: () => menuAction.onSelect?.(),
-          icon: MenuIcon,
-          class: 'bits-pattern-header__title-menu-item--danger',
-          iconClass: 'bits-pattern-header__title-menu-item-icon',
-        },
-      ]
-    : [],
-)
+const menuItems = $derived(menuAction?.items ?? [])
 </script>
 
 <div class="bits-pattern-header__title-group">
@@ -47,17 +36,23 @@ const menuItems = $derived(
   />
 
   {#if menuAction?.visible}
-    <Dropdown
-      ariaLabel={menuAction.label}
-      triggerClass="bits-pattern-header__title-menu-trigger"
-      triggerIcon={ChevronDown}
-      triggerIconClass="bits-pattern-header__title-menu-trigger-icon"
-      contentClass="bits-pattern-header__title-menu-content"
-      contentSide="bottom"
-      contentSideOffset={8}
-      contentAlign="center"
-      itemClass="bits-pattern-header__title-menu-item"
-      items={menuItems}
-    />
+    <div
+      class="bits-pattern-header__title-menu-slot"
+      in:slide={{ axis: "x", duration: 260 }}
+      out:slide={{ axis: "x", duration: 260 }}
+    >
+      <Dropdown
+        ariaLabel={menuAction.ariaLabel ?? text ?? 'Header menu'}
+        triggerClass="bits-pattern-header__title-menu-trigger"
+        triggerIcon={ChevronDown}
+        triggerIconClass="bits-pattern-header__title-menu-trigger-icon"
+        contentClass="bits-pattern-header__title-menu-content"
+        contentSide="bottom"
+        contentSideOffset={8}
+        contentAlign="center"
+        itemClass="bits-pattern-header__title-menu-item"
+        items={menuItems}
+      />
+    </div>
   {/if}
 </div>
