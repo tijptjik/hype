c64d29068c24ad17f2c68cf0143646a2a7366023
Mart van de Ven <m@type.hk>
2026-03-29 21:35:34 +0800

    feat: Add CameraViewer component
    Introduces a new `CameraViewer.svelte` component to display images and
    capture new ones. This component leverages `AdminViewer` and
    `CameraCaptureButton` from the bits UI library.

---
+++ src/lib/bits/patterns/images/CameraViewer.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+import * as ImagePrimitive from './components'
+import AdminViewer from './AdminViewer.svelte'
+import type { CameraViewerProps } from './images.types'
+
+let {
+  items,
+  activeId = $bindable<string | null>(null),
+  fit = 'fit',
+  uploadSelectionMode = 'multiple',
+  leftRail,
+  centerRail,
+  rightRail,
+  isEmptyUploadEnabled = true,
+  onActiveIdChange,
+  onUploadFiles,
+  onCaptureFiles,
+}: CameraViewerProps = $props()
+</script>
+
+<div class="relative h-full w-full">
+  <AdminViewer
+    {items}
+    {activeId}
+    {fit}
+    {uploadSelectionMode}
+    {leftRail}
+    {centerRail}
+    {rightRail}
+    {isEmptyUploadEnabled}
+    {onActiveIdChange}
+    {onUploadFiles}
+  />
+
+  <div class="pointer-events-none absolute bottom-4 left-4 z-40">
+    <div class="pointer-events-auto">
+      <ImagePrimitive.CameraCaptureButton onFiles={onCaptureFiles ?? onUploadFiles} />
+    </div>
+  </div>
+</div>
