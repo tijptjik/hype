f94f317982c53f80cc1b93116a981c119b60a328
Mart van de Ven <m@type.hk>
2026-03-30 02:29:43 +0800

    feat: add HiddenInputs

---
+++ src/lib/bits/custom/form/HiddenInputs.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+// BITS
+import { cx } from '$lib/bits/utils'
+
+type HiddenInputAttrs = Record<string, string | number | boolean | null | undefined>
+
+let {
+  inputs = [],
+  class: className = '',
+}: {
+  inputs?: HiddenInputAttrs[]
+  class?: string
+} = $props()
+
+const rootClass = $derived(cx('hidden', className))
+</script>
+
+<div class={rootClass} aria-hidden="true">
+  {#each inputs as inputAttrs, index (inputAttrs.name ?? index)}
+    <input type="hidden" {...inputAttrs}>
+  {/each}
+</div>
