060ecf4a3868d7f0db792851fddd52e8d5047d72
Mart van de Ven <m@type.hk>
2026-04-04 18:38:14 +0800

    refactor: port labels to bits UI

---
+++ src/lib/bits/patterns/cards/featureCard/components/actions/labels/AddPhotoLabel.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+// I18N
+import { m } from '$lib/i18n'
+// CONTEXT
+import { getImageCtx } from '$lib/context/image.svelte'
+// LOCAL
+import FeatureCardActionLabel from '../FeatureCardActionLabel.svelte'
+
+const imageCtx = getImageCtx()
+const stagedCount = $derived(imageCtx.state.stagingQueue.length)
+const label = $derived(stagedCount > 0 ? m.photo({ n: stagedCount }) : 'Add Photos')
+</script>
+
+<FeatureCardActionLabel text={label} count={stagedCount > 0 ? stagedCount : null} />
---
+++ src/lib/bits/patterns/cards/featureCard/components/actions/labels/index.ts
@@ -0,0 +1,3 @@
+export { default as AddPhotoLabel } from './AddPhotoLabel.svelte'
+export { default as MissingReportLabel } from './MissingReportLabel.svelte'
+export { default as NewFeatureLabel } from './NewFeatureLabel.svelte'
