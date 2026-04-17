03ee7e73550393db93f2a5506e8bea8ee74df0f0
Mart van de Ven <m@type.hk>
2026-04-18 04:29:52 +0800

    refactor: remove redundant labels

--- src/lib/bits/patterns/cards/featureCard/components/actions/labels/MissingReportLabel.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-// I18N
-import { m } from '$lib/i18n'
-// LOCAL
-import FeatureCardActionLabel from '../FeatureCardActionLabel.svelte'
-</script>
-
-<FeatureCardActionLabel text={m.sour_minor_lamb_cry()} />
